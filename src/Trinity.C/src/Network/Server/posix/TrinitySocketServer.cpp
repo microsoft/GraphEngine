5f2c07bae801bde45f53ba813080a37ddb43be9d
Yatao Li <yatli@microsoft.com>
2017-02-16 21:01:33 +0800

    Trinity.C: posix: move PSCO management routines to the source file

--- src/Trinity.C/src/Network/Server/posix/TrinitySocketServer.cpp
+++ src/Trinity.C/src/Network/Server/posix/TrinitySocketServer.cpp
@@ -30,7 +30,9 @@ namespace Trinity
     {
         TrinitySpinlock psco_spinlock; // psco = per socket context object
         std::map<int, PerSocketContextObject*> psco_map;
+        std::atomic<size_t> g_threadpool_size;
 
+#pragma region PerSocketContextObject management
         void AddPerSocketContextObject(PerSocketContextObject * pContext)
         {
             psco_spinlock.lock();
@@ -53,8 +55,82 @@ namespace Trinity
             return pContext;
         }
 
+        PerSocketContextObject* AllocatePerSocketContextObject(int fd)
+        {
+            PerSocketContextObject* p = (PerSocketContextObject*)malloc(sizeof(PerSocketContextObject));
+
+            p->RecvBuffer = (char*)malloc(UInt32_Contants::RecvBufferSize);
+            p->RecvBufferLen = UInt32_Contants::RecvBufferSize;
+            p->avg_RecvBufferLen = UInt32_Contants::RecvBufferSize;
+
+            p->fd = fd;
+            p->WaitingHandshakeMessage = true;
+
+            return p;
+        }
+
+        void FreePerSocketContextObject(PerSocketContextObject* p)
+        {
+            free(p->RecvBuffer);
+            free(p);
+        }
+
+        void ResetContextObjects(PerSocketContextObject * pContext)
+        {
+            free(pContext->Message);
+
+            // Calculate average received message length with a sliding window.
+            pContext->avg_RecvBufferLen = (uint32_t)(pContext->avg_RecvBufferLen * Float_Constants::AvgSlideWin_a + pContext->ReceivedMessageBodyBytes * Float_Constants::AvgSlideWin_b);
+            // Make sure that average received message length is capped above default value.
+            pContext->avg_RecvBufferLen = std::max(pContext->avg_RecvBufferLen, static_cast<uint32_t>(UInt32_Contants::RecvBufferSize));
+            // If the average received message length drops below half of current recv buf len, adjust it.
+            if (pContext->avg_RecvBufferLen < pContext->RecvBufferLen / Float_Constants::AvgSlideWin_r)
+            {
+                free(pContext->RecvBuffer);
+                pContext->RecvBufferLen = pContext->avg_RecvBufferLen;
+                pContext->RecvBuffer = (char*)malloc(pContext->RecvBufferLen);
+            }
+        }
+#pragma endregion
+
+        void CloseClientConnection(PerSocketContextObject* pContext, bool lingering)
+        {
+            RemovePerSocketContextObject(pContext);
+            //TODO lingering
+            close(pContext->fd);
+            FreePerSocketContextObject(pContext);
+        }
+
+        void CheckHandshakeResult(PerSocketContextObject* pContext)
+        {
+            if (pContext->ReceivedMessageBodyBytes != HANDSHAKE_MESSAGE_LENGTH)
+            {
+                Diagnostics::WriteLine(Diagnostics::LogLevel::Error, "ServerSocket: Client {0} responds with invalid handshake message header.", pContext);
+                goto handshake_check_fail;
+            }
+
+            if (memcmp(pContext->Message, HANDSHAKE_MESSAGE_CONTENT, HANDSHAKE_MESSAGE_LENGTH) != 0)
+            {
+                Diagnostics::WriteLine(Diagnostics::LogLevel::Error, "ServerSocket: Client {0} responds with invalid handshake message.", pContext);
+                goto handshake_check_fail;
+            }
+
+            // handshake_check_success: acknowledge the handshake and then switch into recv mode
+            pContext->WaitingHandshakeMessage = false;
+            pContext->Message = (char*)malloc(sizeof(int32_t));
+            pContext->RemainingBytesToSend = sizeof(int32_t);
+            *(int32_t*)pContext->Message = (int32_t)TrinityErrorCode::E_SUCCESS;
+            SendResponse(pContext);
+            return;
+
+        handshake_check_fail:
+            CloseClientConnection(pContext, false);
+            return;
+        }
+
         int StartSocketServer(uint16_t port)
         {
+            g_threadpool_size = 0;
             int sock_fd = -1;
             struct addrinfo hints, *addrinfos, *addrinfop;
             memset(&hints, 0, sizeof(struct addrinfo));
--- src/Trinity.C/src/Network/Server/posix/TrinitySocketServer.h
+++ src/Trinity.C/src/Network/Server/posix/TrinitySocketServer.h
@@ -55,91 +55,22 @@ namespace Trinity
             bool WaitingHandshakeMessage;
         }PerSocketContextObject;
 
-        inline PerSocketContextObject* AllocatePerSocketContextObject(int fd)
-        {
-            PerSocketContextObject* p = (PerSocketContextObject*)malloc(sizeof(PerSocketContextObject));
-
-            p->RecvBuffer = (char*)malloc(UInt32_Contants::RecvBufferSize);
-            p->RecvBufferLen = UInt32_Contants::RecvBufferSize;
-            p->avg_RecvBufferLen = UInt32_Contants::RecvBufferSize;
-
-            p->fd = fd;
-            p->WaitingHandshakeMessage = true;
-
-            return p;
-        }
-
-        inline void FreePerSocketContextObject(PerSocketContextObject* p)
-        {
-            free(p->RecvBuffer);
-            free(p);
-        }
-
-        inline void ResetContextObjects(PerSocketContextObject * pContext)
-        {
-            free(pContext->Message);
-
-            // Calculate average received message length with a sliding window.
-            pContext->avg_RecvBufferLen = (uint32_t)(pContext->avg_RecvBufferLen * Float_Constants::AvgSlideWin_a + pContext->ReceivedMessageBodyBytes * Float_Constants::AvgSlideWin_b);
-            // Make sure that average received message length is capped above default value.
-            pContext->avg_RecvBufferLen = std::max(pContext->avg_RecvBufferLen, static_cast<uint32_t>(UInt32_Contants::RecvBufferSize));
-            // If the average received message length drops below half of current recv buf len, adjust it.
-            if (pContext->avg_RecvBufferLen < pContext->RecvBufferLen / Float_Constants::AvgSlideWin_r)
-            {
-                free(pContext->RecvBuffer);
-                pContext->RecvBufferLen = pContext->avg_RecvBufferLen;
-                pContext->RecvBuffer = (char*)malloc(pContext->RecvBufferLen);
-            }
-        }
-
-        void AddPerSocketContextObject(PerSocketContextObject * pContext);
-        void RemovePerSocketContextObject(int fd);
+        PerSocketContextObject* AllocatePerSocketContextObject(int fd);
+        void                    FreePerSocketContextObject(PerSocketContextObject* p)
+        void                    ResetContextObjects(PerSocketContextObject * pContext)
+        void                    AddPerSocketContextObject(PerSocketContextObject * pContext);
+        void                    RemovePerSocketContextObject(int fd);
         PerSocketContextObject* GetPerSocketContextObject(int fd);
-
-        inline void CloseClientConnection(PerSocketContextObject* pContext, bool lingering)
-        {
-            RemovePerSocketContextObject(pContext);
-            //TODO lingering
-            close(pContext->fd);
-            FreePerSocketContextObject(pContext);
-        }
-
-        inline void CheckHandshakeResult(PerSocketContextObject* pContext)
-        {
-            if (pContext->ReceivedMessageBodyBytes != HANDSHAKE_MESSAGE_LENGTH)
-            {
-                Diagnostics::WriteLine(Diagnostics::LogLevel::Error, "ServerSocket: Client {0} responds with invalid handshake message header.", pContext);
-                goto handshake_check_fail;
-            }
-
-            if (memcmp(pContext->Message, HANDSHAKE_MESSAGE_CONTENT, HANDSHAKE_MESSAGE_LENGTH) != 0)
-            {
-                Diagnostics::WriteLine(Diagnostics::LogLevel::Error, "ServerSocket: Client {0} responds with invalid handshake message.", pContext);
-                goto handshake_check_fail;
-            }
-
-            // handshake_check_success: acknowledge the handshake and then switch into recv mode
-            pContext->WaitingHandshakeMessage = false;
-            pContext->Message = (char*)malloc(sizeof(int32_t));
-            pContext->RemainingBytesToSend = sizeof(int32_t);
-            *(int32_t*)pContext->Message = (int32_t)TrinityErrorCode::E_SUCCESS;
-            SendResponse(pContext);
-            return;
-
-        handshake_check_fail:
-            CloseClientConnection(pContext, false);
-            return;
-        }
+        void                    CloseClientConnection(PerSocketContextObject* pContext, bool lingering);
+        void                    CheckHandshakeResult(PerSocketContextObject* pContext);
 
 
-        int InitializeEventMonitor(int sfd);
-
-        int AcceptConnection(int sock_fd);
-
-        bool ProcessRecv(PerSocketContextObject* pContext);
-
-        bool RearmFD(int fd);
-
+#pragma region Platform-specific routines
+        int                     InitializeEventMonitor(int sfd);
+        int                     AcceptConnection(int sock_fd);
+        bool                    ProcessRecv(PerSocketContextObject* pContext);
+        bool                    RearmFD(int fd);
+#pragma endregion
     }
 }
 
