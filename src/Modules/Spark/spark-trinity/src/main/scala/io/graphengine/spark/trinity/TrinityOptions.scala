3209807ddffaf6991e7204141b986e6fa49d4f2c
Leo Xia <lexi@microsoft.com>
2017-02-28 15:26:46 +0800

    Add copyright head

--- src/Modules/Spark/spark-trinity/src/main/scala/io/graphengine/spark/trinity/TrinityOptions.scala
+++ src/Modules/Spark/spark-trinity/src/main/scala/io/graphengine/spark/trinity/TrinityOptions.scala
@@ -1,3 +1,7 @@
+// Graph Engine
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
 package io.graphengine.spark.trinity
 
 class TrinityOptions(@transient private val parameters: Map[String, String]) extends Serializable{
