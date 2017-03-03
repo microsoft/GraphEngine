1768e07303240726a8b6972c0161904323d64b86
Leo Xia <lexi@microsoft.com>
2017-03-03 16:07:07 +0800

    Check if existing before downloading nuget.exe

--- tools/build.bat
+++ tools/build.bat
@@ -2,10 +2,15 @@ if [%REPO_ROOT%] == [] (
   set REPO_ROOT=%cd%
 )
 
+setlocal enabledelayedexpansion
+
 set MSBUILD_EXE="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 set NUGET_EXE=%REPO_ROOT%\tools\NuGet.exe
-powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile %NUGET_EXE%"
-if %errorlevel% neq 0 exit /b %errorlevel%
+
+if not exist %NUGET_EXE% (
+  powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile %NUGET_EXE%"
+  if !errorlevel! neq 0 exit /b !errorlevel!
+)
 
 set TRINITY_C_SLN=%REPO_ROOT%\src\Trinity.C\Trinity.C.sln
 set TRINITY_CORE_SLN=%REPO_ROOT%\src\Trinity.Core\Trinity.Core.sln
