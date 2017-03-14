c6bf36a6baec331e0bb753d521ec0ac3a489d9c6
Wentao Xu <xuwt6@mail2.sysu.edu.cn>
2017-03-14 09:46:28 +0800

    DataImporter: Process .zip file which contains the same type of files

--- samples/DataImporter/GraphEngine.DataImporter/FileReader.cs
+++ samples/DataImporter/GraphEngine.DataImporter/FileReader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.IO.MemoryMappedFiles;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +28,7 @@ namespace GraphEngine.DataImporter
             string filetype = Path.GetExtension(filename).ToLower();
 
             CompressionMode mode = CompressionMode.None;
-
+            long counter = 0;
             if (filetype == ".gz")
             {
                 mode = CompressionMode.GZip;
@@ -35,9 +36,27 @@ namespace GraphEngine.DataImporter
             else if (filetype == ".zip")
             {
                 mode = CompressionMode.Zip;
-            }//TODO more
-
-            long counter = 0;
+                {
+                    using (ZipArchive archive = ZipFile.Open(path, ZipArchiveMode.Read))
+                    {
+                        foreach (ZipArchiveEntry entry in archive.Entries)
+                        {
+                            using (var sr = new StreamReader(entry.Open()))
+                            {
+                                string s;
+                                while ((s = sr.ReadLine()) != null)
+                                {
+                                    if (++counter % 1000000 == 0)
+                                    {
+                                        Log.WriteLine("Loaded {0} lines...", counter);
+                                    }
+                                    yield return s;
+                                }
+                            }
+                        }
+                    }
+                }
+            }           
             using (var fs = File.Open(path, FileMode.Open, FileAccess.Read))
             using (var stream = GetInputStream(fs, mode))
             using (var sr = new StreamReader(stream))
@@ -65,6 +84,29 @@ namespace GraphEngine.DataImporter
                 case CompressionMode.GZip:
                     return new GZipStream(new BufferedStream(fs), System.IO.Compression.CompressionMode.Decompress);
                 case CompressionMode.Zip:
+                    //{
+                    //    BufferedStream stream = null;
+                    //    long count = 0;
+                    //    using (ZipArchive archive = new ZipArchive(new BufferedStream(fs), ZipArchiveMode.Read))
+                    //    {
+                    //        foreach (ZipArchiveEntry entry in archive.Entries)
+                    //        {
+
+                    //            BufferedStream s = new BufferedStream(entry.Open());
+                    //            if (count == 0)
+                    //            {
+                    //                stream = s;
+                    //            }
+                    //            else
+                    //            {
+                    //                s.CopyTo(stream);
+                    //            }
+                    //            count++;
+                    //        } 
+
+                    //    }
+                    //    return  stream;
+                    //}
                     //TODO zip have directory structure
                     throw new NotImplementedException();
                 default:
