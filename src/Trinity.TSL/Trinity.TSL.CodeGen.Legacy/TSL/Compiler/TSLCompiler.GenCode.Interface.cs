5c0d0ff80b8b06b20a76a8b83d1106048f227b19
Yatao Li <yatli@microsoft.com>
2017-03-18 19:16:07 +0800

    cleanup.

--- src/Trinity.TSL/Trinity.TSL.CodeGen.Legacy/TSL/Compiler/TSLCompiler.GenCode.Interface.cs
+++ src/Trinity.TSL/Trinity.TSL.CodeGen.Legacy/TSL/Compiler/TSLCompiler.GenCode.Interface.cs
@@ -67,69 +67,6 @@ namespace Trinity.TSL
             cw += Verbatim.GetFooter();
             commit_source_file(src_protocol);
 
-            //reset ret
-            cw = Verbatim.GetHeader(script);
-            cw += default_namespace;
-            cw +=
-@"
-{
-";
-            // Generate code for extension method in LocalStorage and MemoryCloud
-            cw += StorageCodeTemplate.GenerateStorageIntegrationCode(script);
-            cw +=
-@"
-}
-";
-            cw += Verbatim.GetFooter();
-            commit_source_file(src_storage);
-
-
-            //reset ret
-            cw = Verbatim.GetHeader(script);
-            cw += default_namespace;
-            cw +=
-@"
-{
-";
-            List<string> external_namespaces = new List<string>();
-            if (included_src_files.Count > 0)
-            {
-                cw = "using Trinity.External;\r\n" + cw;
-            }
-
-            StringBuilder str_builder = new StringBuilder();
-            foreach (var src in included_src_files)
-            {
-                string[] lines = File.ReadAllLines(src);
-                {
-                    foreach (var line in lines)
-                    {
-                        if (line.StartsWith("namespace", StringComparison.Ordinal))
-                        {
-                            str_builder.AppendLine("namespace Trinity.External");
-                            string[] parts = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                            external_namespaces.Add(parts[1].Trim());
-                        }
-                        else
-                        {
-                            string new_line = line;
-                            foreach (var ns in external_namespaces)
-                            {
-                                new_line = line.Replace(ns, "Trinity.External");
-                            }
-                            str_builder.AppendLine(new_line);
-                        }
-                    }
-                }
-            }
-            cw += str_builder.ToString();
-            cw +=
-@"
-}
-";
-            cw += Verbatim.GetFooter();
-            commit_source_file(src_external);
-
             return source_files;
         }
     }
