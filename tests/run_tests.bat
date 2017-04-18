c26418e26e62cb44473df10f2eca421a5539e723
Yatao Li <yatli@microsoft.com>
2017-04-18 16:15:34 +0800

    test: add run_tests scripts

---
+++ tests/run_tests.bat
@@ -0,0 +1,11 @@
+if [%REPO_ROOT%] == [] (
+  set REPO_ROOT=%cd%
+)
+
+set MSBUILD_EXE="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+set NUGET_EXE="%REPO_ROOT%\tools\NuGet.exe"
+set DOTNET_EXE="dotnet"
+
+%NUGET_EXE% restore %REPO_ROOT%\tests\test.csproj -PackagesDirectory %REPO_ROOT%\tests\packages
+%MSBUILD_EXE% %REPO_ROOT%\tests\test.csproj
+%DOTNET_EXE% build %REPO_ROOT%\tests\test_coreclr.csproj
--- tools/run_tests.bat
+++ tools/run_tests.bat
@@ -6,3 +6,6 @@ set SPARK_MODULE_ROOT=%REPO_ROOT%\src\Modules\Spark
 
 :: Run Spark module tests
 call %SPARK_MODULE_ROOT%\run_tests.bat
+
+:: Run GraphEngine tests
+call %REPO_ROOT%\tests\run_tests.bat
