6873d874e744a5fa90a1bfeaa20903e1dd205e81
Yatao Li <yatli@microsoft.com>
2017-04-18 17:03:58 +0800

    tests: write xunit reports to xml files

--- tools/build_coreclr.bat
+++ tools/build_coreclr.bat
@@ -5,6 +5,12 @@ if [%REPO_ROOT%] == [] (
 setlocal enabledelayedexpansion
 
 set MSBUILD_EXE="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+set NUGET_EXE="%REPO_ROOT%\tools\NuGet.exe"
+
+if not exist %NUGET_EXE% (
+  powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile %NUGET_EXE%"
+  if !errorlevel! neq 0 exit /b !errorlevel!
+)
 
 set TRINITY_C_SLN=%REPO_ROOT%\src\Trinity.C\Trinity.C.sln
 set TRINITY_CORE_SLN=%REPO_ROOT%\src\Trinity.Core\Trinity.Core.NETStandard.sln
@@ -27,3 +33,6 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 :: Run dotnet to pack GraphEngine.CoreCLR nuget package
 dotnet pack %TRINITY_CORE_SLN%
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+:: Register local nuget source
+%NUGET_EXE% sources Add -Name "Graph Engine OSS Local" -Source %REPO_ROOT%\bin\
