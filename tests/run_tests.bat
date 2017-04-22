642b5d4a8814180f9e2322a26d12655e4caf8398
Yatao Li <yatli@microsoft.com>
2017-04-22 11:21:12 +0800

    restore packages before running tests

--- tests/run_tests.bat
+++ tests/run_tests.bat
@@ -6,6 +6,6 @@ set MSBUILD_EXE="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 set NUGET_EXE="%REPO_ROOT%\tools\NuGet.exe"
 set DOTNET_EXE="dotnet"
 
-::	%NUGET_EXE% restore %REPO_ROOT%\tests\test.csproj -PackagesDirectory %REPO_ROOT%\tests\packages
+%NUGET_EXE% restore %REPO_ROOT%\tests\test.csproj -PackagesDirectory %REPO_ROOT%\tests\packages
 %MSBUILD_EXE% %REPO_ROOT%\tests\test.csproj
 ::	%DOTNET_EXE% build %REPO_ROOT%\tests\test_coreclr.csproj
