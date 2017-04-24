5120ce4023f3fc1896ac5f866da93ef223d81e9a
Yatao Li <yatli@microsoft.com>
2017-04-24 17:19:59 +0800

    add tests

--- tests/config/config1/config1.cs
+++ tests/config/config1/config1.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.IO;
 using Xunit;
 using Trinity;
 using Trinity.Storage;
@@ -18,15 +19,31 @@ namespace config1
             TrinityConfig.SaveConfig();
         }
         [Fact]
+        public void save_config_no_exception_2()
+        {
+            TrinityConfig.SaveConfig("test2.xml");
+        }
+        [Fact]
         public void load_config_no_exception()
         {
             TrinityConfig.LoadConfig();
+        }
+        [Fact]
+        public void load_config_no_exception_2()
+        {
+            TrinityConfig.LoadConfig("test2.xml");
         }
         [Fact]
         // https://github.com/Microsoft/GraphEngine/issues/22
         public void can_set_non_accessible_storage_root()
         {
             TrinityConfig.StorageRoot = "Y:";
+        }
+        [Fact]
+        public void saves_2_0_config_format()
+        {
+            TrinityConfig.SaveConfig("test.xml");
+            Assert.True(File.ReadAllText("test.xml").Contains("ConfigVersion=\"2.0\""));
         }
     }
 }
