ccc2f72a14f27094d41d78db6fecf3d7ca20365c
leasunhy <leasunhy@gmail.com>
2017-05-01 20:49:30 +0800

    Test: NUnit test runner init

---
+++ tests/nunit_test_runner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using NUnitLite;
+using Trinity.Core;
+using NUnit.Framework;
+
+namespace nunitlite_runner
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("Usage: runner <test-assembly> [nunit-arg ...]");
+                return 1;
+            }
+            var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(args[0]);
+            return new AutoRun(assembly).Execute(args.Skip(1).ToArray());
+        }
+    }
+}
