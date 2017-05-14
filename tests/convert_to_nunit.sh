c953605e0c6904fbe4317ad3e020446975d831a1
leasunhy <leasunhy@gmail.com>
2017-05-14 13:52:25 +0800

    Test: update script

--- tests/convert_to_nunit.sh
+++ tests/convert_to_nunit.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-find . -name 'packages.config' \
-       -not -path './packages.config' \
-       -not -path './template/standalone/packages.config' \
-       -not -path './minimal/*' \
-       -not -path './minimal-tsl/*' \
-    | while read line
-do
-    project_dir=$(dirname "$line")
-    project_name=$(basename "$project_dir")
-    find "$project_dir" -name "*.cs" -not -path '*AssemblyInfo.cs' -not -path '*/obj/*.cs' | xargs sed -i -f xunit2nunit.sed
-    sed "s/test_name/$project_name/g" template/standalone/test_name.csproj > "${project_dir}/${project_name}.csproj"
-    sed "s/test_name/$project_name/g" template/standalone/test_name_coreclr.csproj > "${project_dir}/${project_name}_coreclr.csproj"
-    cp template/standalone/packages.config "$project_dir/"
-    echo $project_name
+for set in unittests stresstests; do
+    find "$set" -name 'packages.config' \
+           -not -path 'unittests/minimal/*' \
+           -not -path 'unittests/minimal-tsl/*' \
+        | while read line
+    do
+        project_dir=$(dirname "$line")
+        project_name=$(basename "$project_dir")
+        find "$project_dir" -name "*.cs" -not -path '*AssemblyInfo.cs' -not -path '*/obj/*.cs' | xargs sed -i -f xunit2nunit.sed
+        sed "s/test_name/$project_name/g" template/standalone/test_name.csproj > "${project_dir}/${project_name}.csproj"
+        sed "s/test_name/$project_name/g" template/standalone/test_name_coreclr.csproj > "${project_dir}/${project_name}_coreclr.csproj"
+        cp template/standalone/packages.config "$project_dir/"
+        echo $project_name
+    done
 done
 
