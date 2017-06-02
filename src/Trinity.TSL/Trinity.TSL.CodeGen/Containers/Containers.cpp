efa0b314880cd97cc46f080aa52ad4f030f6dd56
leasunhy <leasunhy@gmail.com>
2017-06-02 14:33:44 +0800

    TSL: fix List<u8string>

--- src/Trinity.TSL/Trinity.TSL.CodeGen/Containers/Containers.cpp
+++ src/Trinity.TSL/Trinity.TSL.CodeGen/Containers/Containers.cpp
@@ -13,11 +13,13 @@ namespace Trinity
     {
         std::string* Array(NFieldType*);
         std::string* List(NFieldType*);
+        void generate_container_code_for(NFieldType * ft, std::set<std::string> &initialized_types, std::string * source);
 
         std::string* Containers(NTSL* ntsl)
         {
             std::string* source = new std::string();
             source->append(R"::(using System;
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using Trinity.Core.Lib;
@@ -25,40 +27,53 @@ using Trinity.TSL;
 using Trinity.TSL.Lib;
 )::");
 			std::set<string> initialized_types;
-            for (auto &ft : *TSLExternalParserDataTypeVector)
+
+            for (auto cell : *ntsl->cellList)
             {
-				if (!ft->is_list() && !ft->is_array()) continue;
+                for (auto field : *cell->fieldList)
+                    generate_container_code_for(field->fieldType, initialized_types, source);
+            }
 
-				auto accessor_name = data_type_get_accessor_name(ft);
-				if (initialized_types.find(accessor_name) != initialized_types.end()) continue;
-				initialized_types.insert(accessor_name);
-
-                std::string* container_code = nullptr;
-                if (ft->is_list())
-                {
-
-                    auto element = ft->listElementType;
-                    if (element->is_atom()) switch (element->atom_token)
-                    {
-                    case T_BYTETYPE: case T_INTTYPE: case T_LONGTYPE: case T_DOUBLETYPE:
-                        /* skip hand-written templates */
-                        continue;
-                    }
-                    container_code = List(ft);
-                }
-                else if (ft->is_array())
-                {
-                    container_code = Array(ft);
-                }
-
-                if (container_code != nullptr)
-                {
-                    source->append(*container_code);
-                    delete container_code;
-                }
+            for (auto struct_ : *ntsl->structList)
+            {
+                for (auto field : *struct_->fieldList)
+                    generate_container_code_for(field->fieldType, initialized_types, source);
             }
 
             return source;
+        }
+
+        void generate_container_code_for(NFieldType *ft, std::set<std::string> &initialized_types, std::string *source)
+        {
+            if (!ft->is_list() && !ft->is_array()) return;
+
+            auto accessor_name = data_type_get_accessor_name(ft);
+            if (initialized_types.find(accessor_name) != initialized_types.end()) return;
+            initialized_types.insert(accessor_name);
+
+            std::string* container_code = nullptr;
+            if (ft->is_list())
+            {
+
+                auto element = ft->listElementType;
+                if (element->is_atom()) switch (element->atom_token)
+                {
+                case T_BYTETYPE: case T_INTTYPE: case T_LONGTYPE: case T_DOUBLETYPE:
+                    /* skip hand-written templates */
+                    return;
+                }
+                container_code = List(ft);
+            }
+            else if (ft->is_array())
+            {
+                container_code = Array(ft);
+            }
+
+            if (container_code != nullptr)
+            {
+                source->append(*container_code);
+                delete container_code;
+            }
         }
     }
 }
