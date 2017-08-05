4b256c8aca8442a36017f03571570dc76b0a165e
Yatao Li <yatli@microsoft.com>
2017-08-05 18:06:54 +0800

    update build script

--- tools/build.bat
+++ tools/build.bat
@@ -34,6 +34,15 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 %MSBUILD_EXE% /p:Configuration=Release %TRINITY_CORE_SLN%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+:: Register local nuget source
+:: calling `nuget sources list` will create the config file if it does not exist
+%NUGET_EXE% sources list
+%NUGET_EXE% sources Remove -Name "Graph Engine OSS Local"
+%NUGET_EXE% sources Add -Name "Graph Engine OSS Local" -Source %REPO_ROOT%\bin\
+:: Clear local nuget cache
+:: for /f %i in ('dir /a:d /s /b %REPO_ROOT%\tests\packages\GraphEngine.Core*') do rmdir /S /Q %i
+:: Ignore local nuget source errors
+
 :: Run nuget to restore nuget packages for LIKQ
 %NUGET_EXE% restore %LIKQ_SLN%
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -49,14 +58,6 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 %MSBUILD_EXE% /p:Configuration=Release %REPO_ROOT%\src\LIKQ\BuildNuGetPkg\BuildNuGetPkg.csproj
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-:: Register local nuget source
-:: calling `nuget sources list` will create the config file if it does not exist
-%NUGET_EXE% sources list
-%NUGET_EXE% sources Remove -Name "Graph Engine OSS Local"
-%NUGET_EXE% sources Add -Name "Graph Engine OSS Local" -Source %REPO_ROOT%\bin\
-:: Clear local nuget cache
-:: for /f %i in ('dir /a:d /s /b %REPO_ROOT%\tests\packages\GraphEngine.Core*') do rmdir /S /Q %i
-:: Ignore local nuget source errors
 :: Run msbuild to build FanoutSearch.Server
 %MSBUILD_EXE% /p:Configuration=Release %REPO_ROOT%\src\LIKQ\FanoutSearch.Server\FanoutSearch.Server.csproj
 if %errorlevel% neq 0 exit /b %errorlevel%
