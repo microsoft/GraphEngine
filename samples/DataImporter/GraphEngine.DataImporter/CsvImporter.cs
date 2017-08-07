72c36aeb1b6813e3fa296428a484cc827714a694
Yatao Li <yatli@microsoft.com>
2017-08-07 22:10:48 +0800

    manually merge from data importer branch

---
+++ samples/DataImporter/GraphEngine.DataImporter/CsvImporter.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Trinity.Storage;
+namespace GraphEngine.DataImporter
+{
+    class CsvImporter : IImporter<string>
+    {
+        private List<string> m_fieldNames;
+        private JsonImporter m_jsonImporter = new JsonImporter();
+        private CsvParser parser;
+
+        public CsvImporter(char delimiter, bool trim)
+        {
+            parser = new CsvParser(delimiter, trim);   
+        }
+
+        public ICell ImportEntity(string type, string content, long? parent_id = null)
+        {
+            var fields = parser.CsvSplit(content);
+            if (fields.Count != m_fieldNames.Count)
+            {
+                throw new ImporterException("Invalid record. The number of a field ({0}) must equal to {1}: {2}",
+                    fields.Count, m_fieldNames.Count, content);
+            }
+
+            var jsonObj = new JObject();
+            var fieldDescDict = GetFieldDescriptors(type);
+            IFieldDescriptor fieldDesc = null;
+            for (int colIndex = 0; colIndex < m_fieldNames.Count; colIndex++)
+            {
+                var fieldName = m_fieldNames[colIndex];
+                string fieldValue = fields[colIndex];
+
+                if (fieldValue == null || !fieldDescDict.TryGetValue(fieldName, out fieldDesc))
+                {
+                    // Ignore this field if it's value is null or could not find the descriptor.
+                    continue;
+                }
+
+                string csvArraySplitBy = fieldDesc.GetAttributeValue(Consts.c_KW_CsvArray);
+                if (fieldValue == null || csvArraySplitBy == null)
+                {
+                    jsonObj[fieldName] = fieldValue;
+                }
+                else
+                {
+                    var elements = fieldValue.Split(csvArraySplitBy.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    var jsonArray = new JArray();
+                    foreach (var elem in elements)
+                    {
+                        jsonArray.Add(elem);
+                    }
+                    jsonObj[fieldName] = jsonArray;
+                }
+            }
+            return m_jsonImporter.ImportEntity(type, jsonObj.ToString(), parent_id);
+        }
+
+        public IEnumerable<string> PreprocessInput(IEnumerable<string> input)
+        {
+                string headerRow = input.First();
+                m_fieldNames = new List<string>(parser.CsvSplit(headerRow));
+                return input.Skip(1);
+        }
+
+        private static Dictionary<string, IFieldDescriptor> GetFieldDescriptors(string type)
+        {
+            ICellDescriptor cellDesc = Importer.s_cellTypes[type];
+            return cellDesc.GetFieldDescriptors().ToDictionary(_ => _.Name);
+        }
+        public bool IgnoreType()
+        {
+            return false;
+        }
+    }
+}
---
+++ samples/DataImporter/GraphEngine.DataImporter/FileReader.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trinity.Diagnostics;
+
+namespace GraphEngine.DataImporter
+{
+    enum CompressionMode
+    {
+        None,
+        GZip,
+        SevenZip,
+        Zip,
+        Rar,
+        Tar,
+    }
+    class FileReader
+    {
+        internal static IEnumerable<string> ReadFile(string path)
+        {
+            //TODO detect by content
+            string filename = Path.GetFileName(path);
+            string filetype = Path.GetExtension(filename).ToLower();
+
+            CompressionMode mode = CompressionMode.None;
+
+            if (filetype == ".gz")
+            {
+                mode = CompressionMode.GZip;
+            }
+            else if (filetype == ".zip")
+            {
+                mode = CompressionMode.Zip;
+            }//TODO more
+
+            long counter = 0;
+            using (var fs = File.Open(path, FileMode.Open, FileAccess.Read))
+            using (var stream = GetInputStream(fs, mode))
+            using (var sr = new StreamReader(stream))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    if (++counter % 1000000 == 0)
+                    {
+                        Log.WriteLine("Loaded {0} lines...", counter);
+                    }
+                    yield return s;
+                }
+            }
+            Log.WriteLine("Loaded {0} lines.", counter);
+        }
+
+        private static Stream GetInputStream(FileStream fs, CompressionMode mode)
+        {
+            switch (mode)
+            {
+                case CompressionMode.None:
+                    return new BufferedStream(fs);
+                case CompressionMode.GZip:
+                    return new GZipStream(new BufferedStream(fs), System.IO.Compression.CompressionMode.Decompress);
+                case CompressionMode.Zip:
+                    //TODO zip have directory structure
+                    throw new NotImplementedException();
+                default:
+                    throw new NotFiniteNumberException();
+            }
+        }
+    }
+}
