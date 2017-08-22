5ef1dd32fb4de52250b43d3711164f0e7bd2c16a
fexio <fecsio@163.com>
2017-08-22 20:29:50 +0800

    Unstable version for DynamicMemoryCloud

---
+++ src/Trinity.Core/Storage/DynamicMemoryCloud/DynamicMemoryCloud.KVStore.cs
@@ -0,0 +1,210 @@
+﻿// Graph Engine
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using Trinity;
+using Trinity.Network.Messaging;
+
+namespace Trinity.Storage
+{
+    public partial class DynamicMemoryCloud
+    {
+        /// <summary>
+        /// Adds a new cell to the key-value store if the cell Id does not exist, or updates an existing cell in the key-value store if the cell Id already exists.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <param name="buff">A memory buffer that contains the cell content.</param>
+        /// <returns>true if saving succeeds; otherwise, false.</returns>
+        public override TrinityErrorCode SaveCell(long cellId, byte[] buff)
+        {
+            return GetStorageByCellId(cellId).SaveCell(cellId, buff);
+        }
+
+        /// <summary>
+        /// Adds a new cell to the key-value store if the cell Id does not exist, or updates an existing cell in the key-value store if the cell Id already exists.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <param name="buff">A memory buffer that contains the cell content.</param>
+        /// <param name="offset">The byte offset into the buff.</param>
+        /// <param name="size">The size of the cell.</param>
+        /// <returns>true if saving succeeds; otherwise, false.</returns>
+        public override TrinityErrorCode SaveCell(long cellId, byte[] buff, int offset, int size)
+        {
+            return GetStorageByCellId(cellId).SaveCell(cellId, buff, offset, size);
+        }
+
+        /// <summary>
+        /// Adds a new cell to the key-value store if the cell Id does not exist, or updates an existing cell in the key-value store if the cell Id already exists.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <param name="buff">A memory buffer that contains the cell content.</param>
+        /// <param name="size">The size of the cell.</param>
+        /// <returns>true if saving succeeds; otherwise, false.</returns>
+        public override unsafe TrinityErrorCode SaveCell(long cellId, byte* buff, int size)
+        {
+            return GetStorageByCellId(cellId).SaveCell(cellId, buff, size);
+        }
+
+        /// <summary>
+        /// Adds a new cell to the key-value store if the cell Id does not exist, or updates an existing cell in the key-value store if the cell Id already exists.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <param name="buff">A memory buffer that contains the cell content.</param>
+        /// <param name="offset">The byte offset into the buff.</param>
+        /// <param name="size">The size of the cell.</param>
+        /// <param name="cellType">Indicates the cell type.</param>
+        /// <returns>true if saving succeeds; otherwise, false.</returns>
+        public override TrinityErrorCode SaveCell(long cellId, byte[] buff, int offset, int size, ushort cellType)
+        {
+            return GetStorageByCellId(cellId).SaveCell(cellId, buff, offset, size, cellType);
+        }
+
+        /// <summary>
+        /// Adds a new cell to the key-value store if the cell Id does not exist, or updates an existing cell in the key-value store if the cell Id already exists.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <param name="buff">A memory buffer that contains the cell content.</param>
+        /// <param name="cellType">Indicates the cell type.</param>
+        /// <returns>true if saving succeeds; otherwise, false.</returns>
+        public override TrinityErrorCode SaveCell(long cellId, byte[] buff, ushort cellType)
+        {
+            return GetStorageByCellId(cellId).SaveCell(cellId, buff, 0, buff.Length, cellType);
+        }
+
+        /// <summary>
+        /// Loads the bytes of the cell with the specified cell Id.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <param name="cellBuff">The bytes of the cell. An empty byte array is returned if the cell is not found.</param>
+        /// <returns>A Trinity error code. Possible values are E_SUCCESS and E_NOT_FOUND.</returns>
+        public override TrinityErrorCode LoadCell(long cellId, out byte[] cellBuff)
+        {
+            return GetStorageByCellId(cellId).LoadCell(cellId, out cellBuff);
+        }
+
+        /// <summary>
+        /// Loads the type and the content of the cell with the specified cell Id.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <param name="cellBuff">The bytes of the cell. An empty byte array is returned if the cell is not found.</param>
+        /// <param name="cellType">The type of the cell, represented with a 16-bit unsigned integer.</param>
+        /// <returns>A Trinity error code. Possible values are E_SUCCESS and E_NOT_FOUND.</returns>
+        public override TrinityErrorCode LoadCell(long cellId, out byte[] cellBuff, out ushort cellType)
+        {
+            return GetStorageByCellId(cellId).LoadCell(cellId, out cellBuff, out cellType);
+        }
+
+        /// <summary>
+        /// Removes the cell with the specified cell Id from the key-value store.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <returns>true if removing succeeds; otherwise, false.</returns>
+        public override TrinityErrorCode RemoveCell(long cellId)
+        {
+            return GetStorageByCellId(cellId).RemoveCell(cellId);
+        }
+
+        /// <summary>
+        /// Gets the type of the cell with specified cell Id.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <param name="cellType">The type of the cell specified by cellId.</param>
+        /// <returns>A Trinity error code. Possible values are E_SUCCESS and E_NOT_FOUND.</returns>
+        public override unsafe TrinityErrorCode GetCellType(long cellId, out ushort cellType)
+        {
+            return GetStorageByCellId(cellId).GetCellType(cellId, out cellType);
+        }
+
+        /// <summary>
+        /// Determines whether there is a cell with the specified cell Id in Trinity key-value store.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <returns>true if a cell whose Id is cellId is found; otherwise, false.</returns>
+        public override unsafe bool Contains(long cellId)
+        {
+            return GetStorageByCellId(cellId).Contains(cellId);
+        }
+
+        /// <summary>
+        /// Adds a new cell to the Trinity key-value store.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <param name="buff">A memory buffer that contains the cell content.</param>
+        /// <param name="size">The size of the cell.</param>
+        /// <returns>true if adding succeeds; otherwise, false.</returns>
+        public override unsafe TrinityErrorCode AddCell(long cellId, byte* buff, int size)
+        {
+            return GetStorageByCellId(cellId).AddCell(cellId, buff, size);
+        }
+
+        /// <summary>
+        /// Adds a new cell to the Trinity key-value store.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <param name="buff">A memory buffer that contains the cell content.</param>
+        /// <returns>true if adding succeeds; otherwise, false.</returns>
+        public override unsafe TrinityErrorCode AddCell(long cellId, byte[] buff)
+        {
+            return GetStorageByCellId(cellId).AddCell(cellId, buff);
+        }
+
+        /// <summary>
+        /// Adds a new cell to the Trinity key-value store.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <param name="buff">A memory buffer that contains the cell content.</param>
+        /// <param name="offset">The byte offset into the buff.</param>
+        /// <param name="size">The size of the cell.</param>
+        /// <returns>true if adding succeeds; otherwise, false.</returns>
+        public override unsafe TrinityErrorCode AddCell(long cellId, byte[] buff, int offset, int size)
+        {
+            return GetStorageByCellId(cellId).AddCell(cellId, buff, offset, size);
+        }
+
+
+        /// <summary>
+        /// Updates an existing cell in the Trinity key-value store.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <param name="buff">A memory buffer that contains the cell content.</param>
+        /// <param name="size">The size of the cell.</param>
+        /// <returns>true if updating succeeds; otherwise, false.</returns>
+        public override unsafe TrinityErrorCode UpdateCell(long cellId, byte* buff, int size)
+        {
+            return GetStorageByCellId(cellId).UpdateCell(cellId, buff, size);
+        }
+
+        /// <summary>
+        /// Updates an existing cell in the Trinity key-value store.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <param name="buff">A memory buffer that contains the cell content.</param>
+        /// <returns>true if updating succeeds; otherwise, false.</returns>
+        public override unsafe TrinityErrorCode UpdateCell(long cellId, byte[] buff)
+        {
+            return GetStorageByCellId(cellId).UpdateCell(cellId, buff);
+        }
+
+        /// <summary>
+        /// Updates an existing cell in the Trinity key-value store.
+        /// </summary>
+        /// <param name="cellId">A 64-bit cell Id.</param>
+        /// <param name="buff">A memory buffer that contains the cell content.</param>
+        /// <param name="offset">The byte offset into the buff.</param>
+        /// <param name="size">The size of the cell.</param>
+        /// <returns>true if updating succeeds; otherwise, false.</returns>
+        public override unsafe TrinityErrorCode UpdateCell(long cellId, byte[] buff, int offset, int size)
+        {
+            return GetStorageByCellId(cellId).UpdateCell(cellId, buff, offset, size);
+        }
+    }
+}
---
+++ src/Trinity.Core/Storage/DynamicMemoryCloud/DynamicMemoryCloud.Utility.cs
@@ -0,0 +1,113 @@
+﻿// Graph Engine
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using Trinity;
+using Trinity.Core.Lib;
+using Trinity.Network.Messaging;
+using Trinity.Network;
+using Trinity.Utilities;
+using Trinity.Diagnostics;
+using Trinity.Daemon;
+
+namespace Trinity.Storage
+{
+    public unsafe partial class DynamicMemoryCloud
+    {
+        internal unsafe string EchoPing(int serverId, string msg)
+        {
+            TrinityMessage trinity_msg = new TrinityMessage(TrinityMessageType.PRESERVED_SYNC_WITH_RSP, (ushort)RequestType.EchoPing, msg.Length << 1);
+            BitHelper.WriteString(msg, trinity_msg.Buffer + TrinityMessage.Offset);
+
+            TrinityResponse response;
+            SendMessageToServer(serverId, trinity_msg, out response);
+
+            return BitHelper.GetString(response.Buffer + response.Offset, response.Size);
+
+        }
+
+        /// <summary>
+        /// Shutdown the server with the specified serverId.
+        /// </summary>
+        /// <param name="serverId">The id of the server.</param>
+        public unsafe void ShutDown(int serverId)
+        {
+            byte[] message_bytes = new byte[TrinityProtocol.MsgHeader];
+
+            fixed (byte* byte_p = message_bytes)
+            {
+                byte* p = byte_p;
+                *(int*)p = TrinityProtocol.TrinityMsgHeader;
+
+                *(p + TrinityProtocol.MsgTypeOffset) = (byte)TrinityMessageType.PRESERVED_ASYNC;
+
+                *(p + TrinityProtocol.MsgIdOffset) = (byte)RequestType.Shutdown;
+            }
+
+            SendMessageToServer(serverId, message_bytes, 0, message_bytes.Length);
+        }
+
+        internal unsafe void ShutDownProxy(RemoteStorage proxy)
+        {
+            TrinityMessage msg = new TrinityMessage(TrinityMessageType.PRESERVED_ASYNC, (ushort)RequestType.Shutdown, 0);
+            proxy.SendMessage(msg.Buffer, msg.Size);
+        }
+
+        /// <summary>
+        /// Shutdown all running Trinity servers. This only works in the Client mode.
+        /// </summary>
+        public unsafe void ShutDown()
+        {
+            if (TrinityConfig.CurrentRunningMode == RunningMode.Client)
+            {
+                try
+                {
+                    // Trigger the initialization if not initialized yet
+                    var proxies = ProxyList;
+
+                    BackgroundThread.ClearAllTasks(); // To disable heartbeat
+                    Parallel.For(0, ServerCount, i =>
+                    {
+                        ShutDown(i);
+                    }
+                        );
+                    Parallel.ForEach<RemoteStorage>(proxies, proxy =>
+                    {
+                        ShutDownProxy(proxy);
+                        proxy.Dispose();
+                    });
+                    Dispose();
+                    Thread.MemoryBarrier();
+                }
+                catch (Exception ex)
+                {
+                    Log.WriteLine(LogLevel.Error, "Errors occurred during shutdown the Trinity servers.");
+                    Log.WriteLine(LogLevel.Error, ex.ToString());
+                }
+            }
+        }
+
+        public override long GetTotalMemoryUsage()
+        {
+            TrinityMessage tm = new TrinityMessage(TrinityMessageType.PRESERVED_SYNC_WITH_RSP, (ushort)RequestType.QueryMemoryWorkingSet, 0);
+            long[] memUsage = new long[ServerCount];
+            Parallel.For(0, ServerCount, sid =>
+            {
+                TrinityResponse response;
+                SendMessageToServer(sid, tm, out response);
+                memUsage[sid] = *(long*)(response.Buffer + response.Offset);
+            });
+            return memUsage.Sum();
+        }
+    }
+}
