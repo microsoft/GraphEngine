64488927f6c929d656825398ca96caaa4ed993c4
Tavi Truman <TaviTruman@users.noreply.github.com>
2017-08-23 12:56:22 -0700

    Adding in all other aspects of the project for the GE Customer Communications Listener

---
+++ src/Modules/GE Service Fabric Infrastructure/GE.ServiceFabric.Infrastructure.Module/Service.Interface/IGEModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GE.ServiceFabric.Infrastructure.Module.Service.Interface
+{
+    public interface IGEModule : IDisposable
+    {
+        Guid ModuleID { get; }
+        string ModuleVersion { get; }
+        string ModuleName { get; }
+    }
+}
