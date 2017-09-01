448cf53d35388a6bf53da8d336db63a94b03ca78
Leo Xia <lexi@microsoft.com>
2017-09-01 21:54:33 +0800

    update build script for coreclr

--- src/Modules/Spark/build.bat
+++ src/Modules/Spark/build.bat
@@ -4,25 +4,8 @@ if [%REPO_ROOT%] == [] (
   set REPO_ROOT=%cd%\..\..\..
 )
 
-setlocal enabledelayedexpansion
-
-set SPARK_MODULE_ROOT=%REPO_ROOT%\src\Modules\Spark
-
-set VSWHERE_EXE=%REPO_ROOT%\tools\vswhere.exe
-set VS_VERSION=[15.0,16.0)
-set REQUIRES=Microsoft.Component.MSBuild Microsoft.VisualStudio.Component.VC.Tools.x86.x64
-
-for /f "usebackq tokens=*" %%i in (`%VSWHERE_EXE% -version !VS_VERSION! -products * -requires %REQUIRES% -property installationPath`) do (
-  set VS_INSTALLDIR=%%i
-)
-
-set MSBUILD_EXE="%VS_INSTALLDIR%\MSBuild\15.0\Bin\MSBuild.exe"
-if not exist %MSBUILD_EXE% (
-  echo [91m"Visual Studio 2017 or required components (%REQUIRES%) were not found"[0m
-  exit /b 1
-)
-
-set NUGET_EXE=%REPO_ROOT%\tools\NuGet.exe
+call %REPO_ROOT%\tools\setenv.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 %NUGET_EXE% restore %SPARK_MODULE_ROOT%\Spark.sln
 if %errorlevel% neq 0 exit /b %errorlevel%
--- tools/build.bat
+++ tools/build.bat
@@ -1,37 +1,13 @@
-@echo off
-
 if [%REPO_ROOT%] == [] (
   set REPO_ROOT=%~dp0..
 )
 
-setlocal enabledelayedexpansion
+call %REPO_ROOT%\tools\setenv.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-set VSWHERE_EXE=%REPO_ROOT%\tools\vswhere.exe
-set VS_VERSION=[15.0,16.0)
-set REQUIRES=Microsoft.Component.MSBuild Microsoft.VisualStudio.Component.VC.Tools.x86.x64
-
-for /f "usebackq tokens=*" %%i in (`%VSWHERE_EXE% -version !VS_VERSION! -products * -requires %REQUIRES% -property installationPath`) do (
-  set VS_INSTALLDIR=%%i
-)
-
-set MSBUILD_EXE="%VS_INSTALLDIR%\MSBuild\15.0\Bin\MSBuild.exe"
-if not exist %MSBUILD_EXE% (
-  echo [91m"Visual Studio 2017 or required components (%REQUIRES%) were not found"[0m
-  exit /b 1
-)
-
-set NUGET_EXE="%REPO_ROOT%\tools\NuGet.exe"
-
-if not exist %NUGET_EXE% (
-  powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile %NUGET_EXE%"
-  if !errorlevel! neq 0 exit /b !errorlevel!
-)
-
-set TRINITY_C_SLN=%REPO_ROOT%\src\Trinity.C\Trinity.C.sln
-set TRINITY_TSL_SLN=%REPO_ROOT%\src\Trinity.TSL\Trinity.TSL.sln
+setlocal
 set TRINITY_CORE_SLN=%REPO_ROOT%\src\Trinity.Core\Trinity.Core.sln
 set LIKQ_SLN=%REPO_ROOT%\src\Modules\LIKQ\LIKQ.sln
-set SPARK_MODULE_ROOT=%REPO_ROOT%\src\Modules\Spark
 
 :: Run msbuild to build Trinity.C
 %MSBUILD_EXE% /p:Configuration=Release %TRINITY_C_SLN%
@@ -46,6 +22,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Run msbuild to build Trinity.Core
+rd /S /Q %REPO_ROOT%\src\Trinity.Core\obj
 %MSBUILD_EXE% /p:Configuration=Release %TRINITY_CORE_SLN%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
