d9cdf01f4eeb1a3d515852812a87e51586e35c4b
Tavi Truman <TaviTruman@users.noreply.github.com>
2017-11-27 12:58:59 -0800

    update build scripts

--- tools/build.bat
+++ tools/build.bat
@@ -1,13 +1,22 @@
 if [%REPO_ROOT%] == [] (
-  set REPO_ROOT=%~dp0..
+  set REPO_ROOT="%~dp0.."
 )
 
-call %REPO_ROOT%\tools\setenv.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
+setlocal enabledelayedexpansion
 
-setlocal
+set MSBUILD_EXE="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+set NUGET_EXE="%REPO_ROOT%\tools\NuGet.exe"
+
+if not exist %NUGET_EXE% (
+  powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile %NUGET_EXE%"
+  if !errorlevel! neq 0 exit /b !errorlevel!
+)
+
+set TRINITY_C_SLN=%REPO_ROOT%\src\Trinity.C\Trinity.C.sln
+set TRINITY_TSL_SLN=%REPO_ROOT%\src\Trinity.TSL\Trinity.TSL.sln
 set TRINITY_CORE_SLN=%REPO_ROOT%\src\Trinity.Core\Trinity.Core.sln
-set LIKQ_SLN=%REPO_ROOT%\src\Modules\LIKQ\LIKQ.sln
+set LIKQ_SLN=%REPO_ROOT%\src\LIKQ\LIKQ.sln
+set SPARK_MODULE_ROOT=%REPO_ROOT%\src\Modules\Spark
 
 :: Run msbuild to build Trinity.C
 %MSBUILD_EXE% /p:Configuration=Release %TRINITY_C_SLN%
@@ -18,44 +27,41 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Run nuget to restore nuget packages for Trinity.Core
-%NUGET_EXE% restore %TRINITY_CORE_SLN%
+"%NUGET_EXE%" restore %TRINITY_CORE_SLN%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Run msbuild to build Trinity.Core
-rd /S /Q %REPO_ROOT%\src\Trinity.Core\obj
 %MSBUILD_EXE% /p:Configuration=Release %TRINITY_CORE_SLN%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-:: Register local nuget source
-:: calling `nuget sources list` will create the config file if it does not exist
-%NUGET_EXE% sources list
-%NUGET_EXE% sources Remove -Name "Graph Engine OSS Local"
-%NUGET_EXE% sources Add -Name "Graph Engine OSS Local" -Source %REPO_ROOT%\bin\
-:: Clear local nuget cache
-:: for /f %i in ('dir /a:d /s /b %REPO_ROOT%\tests\packages\GraphEngine.Core*') do rmdir /S /Q %i
-:: Ignore local nuget source errors
-
-:: TODO currently LIKQ relies on .NET 4.6 which is not on our build
-:: server. Disable it temporarily now.
 :: Run nuget to restore nuget packages for LIKQ
-:: %NUGET_EXE% restore %LIKQ_SLN%
-:: if %errorlevel% neq 0 exit /b %errorlevel%
+"%NUGET_EXE%" restore %LIKQ_SLN%
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: TODO: Fix the build of LIKQ\FanoutSerach\FanoutSearch.UnitTest
 :: and then build LIKQ.sln as a whole
 
 :: Run msbuild to build FanoutSearch
-:: %MSBUILD_EXE% /p:Configuration=Release %REPO_ROOT%\src\Modules\LIKQ\FanoutSearch\FanoutSearch.csproj
-:: if %errorlevel% neq 0 exit /b %errorlevel%
+%MSBUILD_EXE% /p:Configuration=Release %REPO_ROOT%\src\LIKQ\FanoutSearch\FanoutSearch.csproj
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Run msbuild to build LIKQ nuget package
-:: %MSBUILD_EXE% /p:Configuration=Release %REPO_ROOT%\src\Modules\LIKQ\BuildNuGetPkg\BuildNuGetPkg.csproj
-:: if %errorlevel% neq 0 exit /b %errorlevel%
+%MSBUILD_EXE% /p:Configuration=Release %REPO_ROOT%\src\LIKQ\BuildNuGetPkg\BuildNuGetPkg.csproj
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Run msbuild to build FanoutSearch.Server
-:: %MSBUILD_EXE% /p:Configuration=Release %REPO_ROOT%\src\Modules\LIKQ\FanoutSearch.Server\FanoutSearch.Server.csproj
-:: if %errorlevel% neq 0 exit /b %errorlevel%
+%MSBUILD_EXE% /p:Configuration=Release %REPO_ROOT%\src\LIKQ\FanoutSearch.Server\FanoutSearch.Server.csproj
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Build spark module
 call %SPARK_MODULE_ROOT%\build.bat
-exit /b %errorlevel%
+
+:: Register local nuget source
+:: calling `nuget sources list` will create the config file if it does not exist
+"%NUGET_EXE%" sources list
+"%NUGET_EXE%" sources Remove -Name "Graph Engine OSS Local"
+"%NUGET_EXE%" sources Add -Name "Graph Engine OSS Local" -Source %REPO_ROOT%\bin\
+:: Clear local nuget cache
+:: for /f %i in ('dir /a:d /s /b %REPO_ROOT%\tests\packages\GraphEngine.Core*') do rmdir /S /Q %i
+:: Ignore local nuget source errors
+exit /b 0
