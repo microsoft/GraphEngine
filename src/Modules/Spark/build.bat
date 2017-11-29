d0a2d209951e5d869effab2c6147c885d6a373de
Tavi Truman <TaviTruman@users.noreply.github.com>
2017-11-29 05:27:50 -0800

    Revert "update build scripts"
    This reverts commit d9cdf01f4eeb1a3d515852812a87e51586e35c4b.

--- src/Modules/Spark/build.bat
+++ src/Modules/Spark/build.bat
@@ -1,13 +1,13 @@
+@echo off
+
 if [%REPO_ROOT%] == [] (
-  set REPO_ROOT="%cd%\..\..\.."
+  set REPO_ROOT=%cd%\..\..\..
 )
 
-set SPARK_MODULE_ROOT=%REPO_ROOT%\src\Modules\Spark
+call %REPO_ROOT%\tools\setenv.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-set MSBUILD_EXE="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
-set NUGET_EXE="%REPO_ROOT%\tools\NuGet.exe"
-
-"%NUGET_EXE%" restore %SPARK_MODULE_ROOT%\Spark.sln
+%NUGET_EXE% restore %SPARK_MODULE_ROOT%\Spark.sln
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 %MSBUILD_EXE% /p:Configuration=Release %SPARK_MODULE_ROOT%\Spark.sln
