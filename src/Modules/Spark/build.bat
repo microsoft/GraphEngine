328cb6b140e58efc3c46921c5fd4275341df4380
Yatao Li <yatli@microsoft.com>
2017-11-29 15:21:51 +0800

    update build scripts

--- src/Modules/Spark/build.bat
+++ src/Modules/Spark/build.bat
@@ -1,14 +1,14 @@
 @echo off
 
 if [%REPO_ROOT%] == [] (
-  set REPO_ROOT=%cd%\..\..\..
+  set "REPO_ROOT=%cd%\..\..\.."
 )
 
-call %REPO_ROOT%\tools\setenv.bat
+call "%REPO_ROOT%\tools\setenv.bat"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-%NUGET_EXE% restore %SPARK_MODULE_ROOT%\Spark.sln
+"%NUGET_EXE%" restore "%SPARK_MODULE_ROOT%\Spark.sln"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-%MSBUILD_EXE% /p:Configuration=Release %SPARK_MODULE_ROOT%\Spark.sln
+"%MSBUILD_EXE%" /p:Configuration=Release "%SPARK_MODULE_ROOT%\Spark.sln"
 if %errorlevel% neq 0 exit /b %errorlevel%
--- tools/build.bat
+++ tools/build.bat
@@ -2,35 +2,33 @@ if [%REPO_ROOT%] == [] (
   set REPO_ROOT=%~dp0..
 )
 
-call %REPO_ROOT%\tools\setenv.bat
+call "%REPO_ROOT%\tools\setenv.bat"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 setlocal
-set TRINITY_CORE_SLN=%REPO_ROOT%\src\Trinity.Core\Trinity.Core.sln
-set LIKQ_SLN=%REPO_ROOT%\src\Modules\LIKQ\LIKQ.sln
 
 :: Run msbuild to build Trinity.C
-%MSBUILD_EXE% /p:Configuration=Release %TRINITY_C_SLN%
+"%MSBUILD_EXE%" /p:Configuration=Release "%TRINITY_C_SLN%"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Run msbuild to build Trinity.TSL.CodeGen
-%MSBUILD_EXE% /p:Configuration=Release %TRINITY_TSL_SLN%
+"%MSBUILD_EXE%" /p:Configuration=Release "%TRINITY_TSL_SLN%"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Run nuget to restore nuget packages for Trinity.Core
-%NUGET_EXE% restore %TRINITY_CORE_SLN%
+"%NUGET_EXE%" restore "%TRINITY_CORE_SLN%"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Run msbuild to build Trinity.Core
-rd /S /Q %REPO_ROOT%\src\Trinity.Core\obj
-%MSBUILD_EXE% /p:Configuration=Release %TRINITY_CORE_SLN%
+rd /S /Q "%REPO_ROOT%\src\Trinity.Core\obj"
+"%MSBUILD_EXE%" /p:Configuration=Release "%TRINITY_CORE_SLN%"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Register local nuget source
 :: calling `nuget sources list` will create the config file if it does not exist
-%NUGET_EXE% sources list
-%NUGET_EXE% sources Remove -Name "Graph Engine OSS Local"
-%NUGET_EXE% sources Add -Name "Graph Engine OSS Local" -Source %REPO_ROOT%\bin\
+"%NUGET_EXE%" sources list
+"%NUGET_EXE%" sources Remove -Name "Graph Engine OSS Local"
+"%NUGET_EXE%" sources Add -Name "Graph Engine OSS Local" -Source "%REPO_ROOT%\bin\"
 :: Clear local nuget cache
 :: for /f %i in ('dir /a:d /s /b %REPO_ROOT%\tests\packages\GraphEngine.Core*') do rmdir /S /Q %i
 :: Ignore local nuget source errors
@@ -57,5 +55,5 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 :: if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Build spark module
-call %SPARK_MODULE_ROOT%\build.bat
+call "%SPARK_MODULE_ROOT%\build.bat"
 exit /b %errorlevel%
