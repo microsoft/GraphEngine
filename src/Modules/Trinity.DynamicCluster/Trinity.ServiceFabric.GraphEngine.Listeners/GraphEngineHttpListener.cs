8b62b2dcc83795975ed9c8c60a7dffb4de6ec8be
Tavi Truman <TaviTruman@users.noreply.github.com>
2017-12-06 18:14:13 -0800

    Revised and update Http and TCP Trinity Grpah Engine Listeners

--- src/Modules/Trinity.DynamicCluster/Trinity.ServiceFabric.GraphEngine.Listeners/GraphEngineHttpListener.cs
+++ src/Modules/Trinity.DynamicCluster/Trinity.ServiceFabric.GraphEngine.Listeners/GraphEngineHttpListener.cs
@@ -1,7 +1,40 @@
-﻿namespace Trinity.ServiceFabric.GraphEngine.Listeners
+﻿using System.Diagnostics;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Trinity.ServiceFabric.GarphEngine.Infrastructure;
+
+namespace Trinity.ServiceFabric.GraphEngine.Listeners
 {
-    public class GraphEngineHttpListener
+    public class GraphEngineHttpListener : ICommunicationListener
     {
-        
+        private readonly GraphEngineStatefulServiceRuntime m_graphEngineRuntime;
+
+        private GraphEngineStatefulServiceRuntime GraphEngineRuntime => m_graphEngineRuntime;
+
+        public GraphEngineHttpListener(GraphEngineStatefulServiceRuntime graphEngineRuntime)
+        {
+            this.m_graphEngineRuntime = graphEngineRuntime;
+        }
+
+        public void Abort()
+        {
+            Debug.Assert(GraphEngineRuntime != null, nameof(GraphEngineRuntime) + " != null");
+            GraphEngineRuntime.TrinityServerRuntime.Stop();
+        }
+
+        public Task CloseAsync(CancellationToken cancellationToken)
+        {
+            Debug.Assert(GraphEngineRuntime != null, nameof(GraphEngineRuntime) + " != null");
+            GraphEngineRuntime.TrinityServerRuntime.Stop();
+            return Task.FromResult(0);
+        }
+
+        public Task<string> OpenAsync(CancellationToken cancellationToken)
+        {
+            GraphEngineRuntime.TrinityServerRuntime.Stop();
+            return Task.FromResult($"http://{GraphEngineRuntime.TrinityServerRuntime.Address}:{GraphEngineRuntime.TrinityServerRuntime.HttpPort}");
+        }
     }
 }
--- src/Modules/Trinity.DynamicCluster/Trinity.ServiceFabric.GraphEngine.Listeners/GraphEngineListener.cs
+++ src/Modules/Trinity.DynamicCluster/Trinity.ServiceFabric.GraphEngine.Listeners/GraphEngineListener.cs
@@ -1,7 +1,42 @@
-﻿namespace Trinity.ServiceFabric.GraphEngine.Listeners
+﻿using System.Diagnostics;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Trinity.ServiceFabric.GarphEngine.Infrastructure;
+
+namespace Trinity.ServiceFabric.GraphEngine.Listeners
 {
-    public class GraphEngineListener
+    public class GraphEngineListener : ICommunicationListener
     {
-        
+        private readonly GraphEngineStatefulServiceRuntime m_graphEngineRuntime;
+
+        public GraphEngineListener(GraphEngineStatefulServiceRuntime graphEngineRuntime)
+        {
+            m_graphEngineRuntime = graphEngineRuntime;
+        }
+
+        private GraphEngineStatefulServiceRuntime GraphEngineRuntime => m_graphEngineRuntime;
+
+        /// <inheritdoc />
+        public void Abort()
+        {
+            Debug.Assert(GraphEngineRuntime != null, nameof(GraphEngineRuntime) + " != null");
+            GraphEngineRuntime.TrinityServerRuntime.Stop();
+        }
+
+        public Task CloseAsync(CancellationToken cancellationToken)
+        {
+            Debug.Assert(GraphEngineRuntime != null, nameof(GraphEngineRuntime) + " != null");
+            GraphEngineRuntime.TrinityServerRuntime.Stop();
+            return Task.FromResult(0);
+        }
+
+        public Task<string> OpenAsync(CancellationToken cancellationToken)
+        {
+            Debug.Assert(GraphEngineRuntime != null, nameof(GraphEngineRuntime) + " != null");
+            GraphEngineRuntime.TrinityServerRuntime.Start();
+            return Task.FromResult($"tcp://{GraphEngineRuntime.TrinityServerRuntime.Address}:{GraphEngineRuntime.TrinityServerRuntime.Port}");
+        }
     }
 }
