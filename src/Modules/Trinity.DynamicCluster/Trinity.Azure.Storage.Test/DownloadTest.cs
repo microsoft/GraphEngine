85fa68de8b990656d604d0b85251f61f25af45f8
thautwarm <twshere@outlook.com>
2017-12-13 01:49:06 +0800

    Test for Upload

--- src/Modules/Trinity.DynamicCluster/Trinity.Azure.Storage.Test/DownloadTest.cs
+++ src/Modules/Trinity.DynamicCluster/Trinity.Azure.Storage.Test/DownloadTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.WindowsAzure.Storage.Blob;
@@ -13,21 +16,31 @@ namespace Trinity.Azure.Storage.Test
     {
         private BlobStoragePersistentStorage m_storage;
         private CloudBlobClient m_client;
-        private readonly Guid m_version = Guid.NewGuid();
+        private readonly Guid m_version = new Guid("0939a250-e41e-48b2-bce9-f3195b0388ae");
+        
+        
 
         [TestInitialize]
         public void Init()
         {
+            
             ConfigInit.Init();
             m_storage = new BlobStoragePersistentStorage();
             m_client = m_storage._test_getclient();
+           
+        }
+
+        [TestMethod]
+        public async Task GetLatestVersion()
+        {
             var container = m_client.GetContainerReference(BlobStorageConfig.Instance.ContainerName);
             container.CreateIfNotExists();
             var dir = container.GetDirectoryReference(m_version.ToString());
             Chunk c1 = new Chunk(0, 10);
             Chunk c2 = new Chunk(100, 110);
-            string idx = string.Join("\n", new[] {c1, c2}.Select(JsonConvert.SerializeObject));
+            string idx = string.Join("\n", new[] { c1, c2 }.Select(JsonConvert.SerializeObject));
             dir.GetBlockBlobReference(Constants.c_index).UploadText(idx);
+
             byte[] f1 = new byte[14];
             byte[] f2 = new byte[16];
             f2[0] = 0xEB;
@@ -37,13 +50,21 @@ namespace Trinity.Azure.Storage.Test
             dir.GetBlockBlobReference(c1.Id.ToString()).UploadFromByteArray(f1, 0, f1.Length);
             dir.GetBlockBlobReference(c2.Id.ToString()).UploadFromByteArray(f2, 0, f2.Length);
             dir.GetBlockBlobReference(Constants.c_finished).UploadText("");
+
+            var v = await m_storage.GetLatestVersion();
+          
+            Assert.AreEqual(m_version, v);
         }
 
         [TestMethod]
-        public async Task GetLatestVersion()
+        public async Task Download()
         {
-            var v = await m_storage.GetLatestVersion();
-            Assert.AreEqual(m_version, v);
+            var v = await m_storage.Download(m_version, 0, 110);
+            var src = await v.DownloadAsync();
+            var src_seq = src.GetEnumerator();
+            src_seq.MoveNext();
+            var buffer = src_seq.Current.Buffer;
+
         }
     }
 }
---
+++ src/Modules/Trinity.DynamicCluster/Trinity.Azure.Storage.Test/UploadTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Trinity.Diagnostics;
+using Trinity.DynamicCluster.Persistency;
+using Trinity.Storage;
+
+namespace Trinity.Azure.Storage.Test
+{
+    [TestClass]
+    public class UploadTest
+    {
+        private BlobStoragePersistentStorage m_storage;
+        private CloudBlobClient m_client;
+        private readonly Guid m_version = new Guid("0939a250-e41e-48b2-bce9-f3195b0388ae");
+
+        [TestInitialize]
+        public void Init()
+        {
+            ConfigInit.Init();
+            m_storage = new BlobStoragePersistentStorage();
+        }
+
+        [TestMethod]
+        public async Task Upload()
+        {
+            byte[] data = Encoding.UTF8.GetBytes("I am the bone of my sword.");
+
+            var LowKey = 0;
+            var HighKey = data.Length;
+
+            Console.WriteLine($"HighKey: {HighKey}");
+
+            var uploader = await m_storage.Upload(m_version, LowKey, HighKey);
+
+            Chunk myChunk = new Chunk(LowKey, HighKey, new Guid("68d7c5be-beac-43a6-abf8-4daa4dce9090"));
+            uploader.UploadAsync(new InMemoryDataChunk(myChunk, data, LowKey, HighKey)).Wait();
+            var v = await m_storage.Download(m_version, LowKey, HighKey);
+            var src = await v.DownloadAsync();
+            var src_seq = src.GetEnumerator();
+            
+            var buffer = src_seq.Current.Buffer;
+            Console.WriteLine(Encoding.UTF8.GetString(buffer.Take(data.Length).ToArray()));
+
+            Assert.IsTrue(buffer.Take(data.Length).SequenceEqual(data.Take(data.Length)));
+            
+        }
+    }
+}
