21372080f251accb38fbc06d6a9e747c4323be50
Yatao Li <yatli@microsoft.com>
2018-01-22 13:30:28 +0800

    Update NameService.cs

--- src/Modules/Trinity.DynamicCluster/Trinity.ServiceFabric.GarphEngine.Runtime/Interfaces/NameService.cs
+++ src/Modules/Trinity.DynamicCluster/Trinity.ServiceFabric.GarphEngine.Runtime/Interfaces/NameService.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.ServiceFabric.Data.Collections;
+﻿
 // Graph Engine
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
+using Microsoft.ServiceFabric.Data.Collections;
 using Microsoft.ServiceFabric.Services.Client;
 using Microsoft.ServiceFabric.Services.Runtime;
 using Newtonsoft.Json.Linq;
