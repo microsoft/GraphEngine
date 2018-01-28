745fee2b563dfa795b57ec3e52ecc9f0f8d935a5
Yatao Li <yatli@microsoft.com>
2018-01-28 21:55:47 +0800

    dynamic cluster: fix solution build scripts

--- src/Modules/Trinity.DynamicCluster/Trinity.ServiceFabric.Client/GraphEngineClient.Message.cs
+++ src/Modules/Trinity.DynamicCluster/Trinity.ServiceFabric.Client/GraphEngineClient.Message.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Trinity.Network;
+using Trinity.Network.Messaging;
+using Trinity.Storage;
 
 namespace Trinity.ServiceFabric.Client
 {
-    public partial class GraphEngineClient
+    public partial class GraphEngineClient : IMessagePassingEndpoint
     {
+        public T GetModule<T>() where T : CommunicationModule
+        {
+            throw new NotImplementedException();
+        }
+
+        public unsafe void SendMessage(byte* message, int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public unsafe void SendMessage(byte* message, int size, out TrinityResponse response)
+        {
+            throw new NotImplementedException();
+        }
+
+        public unsafe void SendMessage(byte** message, int* sizes, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public unsafe void SendMessage(byte** message, int* sizes, int count, out TrinityResponse response)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/Modules/Trinity.DynamicCluster/Trinity.ServiceFabric.Client/GraphEngineClient.cs
+++ src/Modules/Trinity.DynamicCluster/Trinity.ServiceFabric.Client/GraphEngineClient.cs
@@ -9,27 +9,50 @@ using Trinity.Storage;
 
 namespace Trinity.ServiceFabric.Client
 {
-    public partial class GraphEngineClient : MemoryCloud, IMessagePassingEndpoint
+    public partial class GraphEngineClient : MemoryCloud
     {
-        public T GetModule<T>() where T : CommunicationModule => GetCommunicationModule<T>();
+        public override int MyInstanceId => throw new NotImplementedException();
 
+        public override int MyPartitionId => throw new NotImplementedException();
 
-        public unsafe void SendMessage(byte* message, int size)
+        public override int MyProxyId => throw new NotImplementedException();
+
+        public override IEnumerable<Chunk> MyChunks => throw new NotImplementedException();
+
+        public override int PartitionCount => throw new NotImplementedException();
+
+        public override int ProxyCount => throw new NotImplementedException();
+
+        public override IList<RemoteStorage> ProxyList => throw new NotImplementedException();
+
+        protected override IList<IStorage> StorageTable => throw new NotImplementedException();
+
+        public override long GetTotalMemoryUsage()
         {
             throw new NotImplementedException();
         }
 
-        public unsafe void SendMessage(byte* message, int size, out TrinityResponse response)
+        public override bool IsLocalCell(long cellId)
         {
             throw new NotImplementedException();
         }
 
-        public unsafe void SendMessage(byte** message, int* sizes, int count)
+        public override bool LoadStorage()
         {
             throw new NotImplementedException();
         }
 
-        public unsafe void SendMessage(byte** message, int* sizes, int count, out TrinityResponse response)
+        public override bool Open(ClusterConfig config, bool nonblocking)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ResetStorage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool SaveStorage()
         {
             throw new NotImplementedException();
         }
