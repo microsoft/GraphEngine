e2643b99ced66e2e3163d0bfd5ec30fa82210178
Yatao Li <yatli@microsoft.com>
2018-01-29 01:23:01 +0800

    dynamic cluster: trinity client module wip

---
+++ src/Modules/Trinity.DynamicCluster/Trinity.Client/IClientRegistry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trinity.Client
+{
+    /// <summary>
+    /// Should be implemented by a hosting memory cloud.
+    /// </summary>
+    public interface IClientRegistry
+    {
+        int RegisterClient(ClientIStorage client);
+        void UnregisterClient(int instanceId);
+    }
+}
