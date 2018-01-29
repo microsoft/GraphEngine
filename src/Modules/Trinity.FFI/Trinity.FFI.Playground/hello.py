97c4078d6b2e4acae8e845b109cb565e1e367768
thautwarm <twshere@outlook.com>
2018-01-29 15:43:18 +0800

    improve the tests for swig FFI-interfaces; fix Python.Storage.core

--- src/Modules/Trinity.FFI/Trinity.FFI.Playground/hello.py
+++ src/Modules/Trinity.FFI/Trinity.FFI.Playground/hello.py
@@ -1,14 +1,48 @@
 import GraphEngine as ge
 
-print("hello")
+global c
+Ignore = object()
+Succeed = []
+Failed = []
 
-c = ge.NewCell("C1")
-c.RemoveField("baz")
-print(c.HasField("baz"))
-d = c.GetField("foo")
-print(d)
-# print(len(d))
-# print(d.__class__, d.__dir__())
-print("world")
-del c
-print("end")
+def test(title, args, out=None, expected=Ignore):
+    print(title)
+    head, *tail = args
+    try:
+        this = head(*tail)
+        print(this)
+        if out:
+            globals()[out] = this
+        if expected is not Ignore:
+            assert expected == this
+        print('finish {}'.format(title))
+        Succeed.append(title)
+    except Exception as e:
+        print('{} failed, error: {}'.format(title, e))
+        Failed.append(title)
+    print('===================')
+    
+print("start-test")
+
+test('[new cell]',
+     [ge.NewCell_1, "C1"], out='c')
+
+test('[has optinal field]', 
+     [c.HasField, 'baz'], expected=1)
+
+test('[remove optional field]', 
+     [c.RemoveField, 'baz'])
+
+test('[has field for removed optional attr]', 
+     [c.HasField, 'baz'])
+
+test('[get trivial field]',
+     [c.GetField, 'foo'], out='d')
+
+test('[delete cell]',
+     [lambda: exec('del c', globals())])
+
+print("end-test")
+
+print('succeed:\n\t','\n\t'.join(Succeed), sep='')
+print('failed:\n\t', '\n\t'.join(Failed),  sep='')
--- src/Modules/Trinity.FFI/Trinity.FFI.Python/PythonBinding/Storage/core/Serialize.py
+++ src/Modules/Trinity.FFI/Trinity.FFI.Python/PythonBinding/Storage/core/Serialize.py
@@ -2,18 +2,33 @@
 """
 Created on Sun Jan 28 21:48:50 2018
 
-@author: yatli
+@author: yatli/thautwarm
 """
 
 import json
+import warnings
 
-Serializer = {}
+Serializer = {int: None,
+              float: None,
+              str: None,
+              dict: None,
+              list: None,
+              tuple: None
+              }
+
+
 def mark_as_serializable(typ, serializing_method):
     Serializer[typ] = serializing_method
 
+
 class TSLJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        return Serializer[obj.__class__](obj)
-    
-        
-    
+    def encode(self, obj):
+        cls = Serializer[obj.__class__]
+        if cls is None:
+            return json.JSONEncoder.encode(self, obj)
+        return cls(obj)
+
+    def default(self, o):
+        warnings.warn(
+                'unsolved type for JSON encoder, use `{}.__str__` as default'.format(o.__class__))
+        return str(o)
