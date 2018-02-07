25c850a803850d94696c59511fec5b3be7670788
Yatao Li <yatli@microsoft.com>
2018-02-08 01:20:44 +0800

    refresh SF smoke test

--- src/Modules/GraphEngine.ServiceFabric/Trinity.ServiceFabric.GraphEngineService/Program.cs
+++ src/Modules/GraphEngine.ServiceFabric/Trinity.ServiceFabric.GraphEngineService/Program.cs
@@ -3,20 +3,27 @@ using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.ServiceFabric.Services.Runtime;
-using Trinity.ServiceFabric.Infrastructure;
 
 namespace Trinity.ServiceFabric
 {
     internal static class Program
     {
+        /// <summary>
+        /// This is the entry point of the service host process.
+        /// </summary>
         private static void Main()
         {
-            Console.WriteLine(Trinity.Azure.Storage.BlobStorageConfig.Instance.ConnectionString);
+            // The ServiceManifest.XML file defines one or more service type names.
+            // Registering a service maps a service type name to a .NET type.
+            // When Service Fabric creates an instance of this service type,
+            // an instance of the class is created in this host process.
+
             ServiceRuntime.RegisterServiceAsync("Trinity.ServiceFabric.GraphEngineServiceType",
                 context => new GraphEngineService(context)).GetAwaiter().GetResult();
 
             // Prevents this host process from terminating so services keep running.
             Thread.Sleep(Timeout.Infinite);
+
         }
     }
 }
