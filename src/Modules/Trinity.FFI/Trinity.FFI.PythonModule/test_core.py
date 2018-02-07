791b4f662968b60acb2874abd0e857ac6013a9a1
thautwarm <twshere@outlook.com>
2018-02-07 19:23:30 +0800

    simple comparison with redis

--- src/Modules/Trinity.FFI/Trinity.FFI.PythonModule/test_core.py
+++ src/Modules/Trinity.FFI/Trinity.FFI.PythonModule/test_core.py
@@ -6,6 +6,8 @@ Created on Wed Feb  7 00:37:40 2018
 """
 
 import GraphEngine as ge
+import redis
+redis_server = redis.StrictRedis()
 gm = ge.GraphMachine('storage')
 gm.start()
 
@@ -18,21 +20,37 @@ print(symtable)
 
 
 # take the Type `C1` from symtable, and use it to new a cell.
-c1 = Cell(symtable['C1'])
-c1.set('foo', 1)
-c1.compute()
-c1.set('foo', 2)
-print(c1)
+#c1 = Cell(symtable['C1'])
+#c1.set('foo', 1)
+#c1.compute()
+#c1.set('foo', 2)
+#print(c1)
+#
+#c2 = Cell(symtable['C2'])
+#c2.set('bar', '123')
+#c2.compute()
+#print(c2)
+#
+#c3 = Cell(symtable['C2'])
+#c3['lst'] = [1, 2, 3]
+#c3.compute()
+#print(c3)
 
-c2 = Cell(symtable['C2'])
-c2.set('bar', '123')
-c2.compute()
-print(c2)
 
-c3 = Cell(symtable['C2'])
-c3['lst'] = [1, 2, 3]
-c3.compute()
-print(c3)
+spec = {'lst': [1, 2, 3, 4, 5],
+        'bar': '123'}
 
+def test_redis():
+    redis_server.append('C2', spec)
+    
+def test_ge():
+    
+    c = Cell(symtable['C2'])
+    
+    for field, value in spec.items():
+        c.set(field, value)
+    
+    c.compute()
 
+    
 
