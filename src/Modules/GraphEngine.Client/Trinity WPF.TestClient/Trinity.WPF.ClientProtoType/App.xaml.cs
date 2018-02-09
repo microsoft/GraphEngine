96edb6e8fef5690164e42d89ef11a1c67290c652
Tavi Truman <TaviTruman@users.noreply.github.com>
2018-02-08 19:43:13 -0800

    New Trinity WPF GE Native and GE/SF Testing Facility

--- src/Modules/GraphEngine.Client/Trinity WPF.TestClient/Trinity.WPF.ClientProtoType/App.xaml.cs
+++ src/Modules/GraphEngine.Client/Trinity WPF.TestClient/Trinity.WPF.ClientProtoType/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Trinity.WPF.ClientProtoType
+namespace Trinity.GE.WPF.TestFacility
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- src/Modules/GraphEngine.Client/Trinity WPF.TestClient/Trinity.WPF.ClientProtoType/MainWindow.xaml.cs
+++ src/Modules/GraphEngine.Client/Trinity WPF.TestClient/Trinity.WPF.ClientProtoType/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Trinity.WPF.ClientProtoType
+namespace Trinity.GE.WPF.TestFacility
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
