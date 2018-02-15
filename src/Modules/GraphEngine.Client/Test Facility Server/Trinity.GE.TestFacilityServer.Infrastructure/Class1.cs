fae9c729f9f818879456e41cb3a2e94641d149b0
Tavi Truman <TaviTruman@users.noreply.github.com>
2018-02-15 05:09:54 -0800

    Updating and adding more aspects of the GE Test Facility Server

---
+++ src/Modules/GraphEngine.Client/Test Facility Server/Trinity.GE.TestFacilityServer.Infrastructure/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trinity.GE.TestFacilityServer.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
