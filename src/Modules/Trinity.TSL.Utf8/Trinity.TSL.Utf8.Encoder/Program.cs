da34700ccfefc43d8a36292443cdb4af04fcd63d
kaiwang13 <719424398@qq.com>
2018-03-26 14:59:12 +0800

    beta version of TSL utf8 support build target

---
+++ src/Modules/Trinity.TSL.Utf8/Trinity.TSL.Utf8.Encoder/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Trinity.TSL.Utf8.Encoder
+{
+    class Program
+    {
+        static string Encode(string input)
+            => new Regex("\"([^\"]*)\"").Replace(input, match => "UTF8Encoded_" + Base64UrlEncoder.Encode(match.Groups[1].Value).Replace('-', '_') + "_UTF8Encoded");
+
+        static string Decode(string input)
+            => new Regex("UTF8Encoded_(.*?)_UTF8Encoded").Replace(input, match => Base64UrlEncoder.Decode(match.Groups[1].Value.Replace('_', '-')));
+
+        static void VisitAllFiles(string rootPath, Action<string> action)
+        {
+            DirectoryInfo root = new DirectoryInfo(rootPath);
+            foreach (DirectoryInfo d in root.GetDirectories())
+            {
+                VisitAllFiles(d.FullName, action);
+            }
+            foreach (FileInfo f in root.GetFiles())
+            {
+                action(f.FullName);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string type = args[0];
+            string inputPath = args[1];
+            if (type == "0")
+            {
+                File.WriteAllText(inputPath + ".tsl", Encode(File.ReadAllText(inputPath)));
+            }
+            else if (type == "1")
+            {
+                VisitAllFiles(inputPath.TrimEnd('\\', '/', '"'), path => File.WriteAllText(path, Decode(File.ReadAllText(path))));
+            }
+        }
+    }
+}
