7a5092558aba9fc27d93d99c0e81073250e4219d
Yatao Li <yatli@microsoft.com>
2018-03-26 18:26:57 +0800

    GraphEngine.TSL.Utf8: nuget package works properly; utf8 conversion does not.

---
+++ src/Modules/GraphEngine.TSL.Utf8/Decoder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Microsoft.IdentityModel.Tokens;
+using Trinity.Utilities;
+
+namespace GraphEngine.TSL.Utf8
+{
+    public class DecoderTask : Microsoft.Build.Utilities.Task
+    {
+        private static string Decode(string input)
+            => new Regex("UTF8Encoded_(.*?)_UTF8Encoded").Replace(input, match => Base64UrlEncoder.Decode(match.Groups[1].Value.Replace('_', '-')));
+
+        [Required]
+        public string SourceFile { get; set; }
+
+        public override bool Execute()
+        {
+            try
+            {
+                File.WriteAllText(SourceFile, Decode(File.ReadAllText(SourceFile)));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.Log.LogErrorFromException(ex);
+                return false;
+            }
+        }
+    }
+}
---
+++ src/Modules/GraphEngine.TSL.Utf8/Encoder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Microsoft.IdentityModel.Tokens;
+using Trinity.Utilities;
+
+namespace GraphEngine.TSL.Utf8
+{
+    public class EncoderTask : Microsoft.Build.Utilities.Task
+    {
+        private static string Encode(string input)
+            => new Regex("\"([^\"]*)\"").Replace(input, match => "UTF8Encoded_" + Base64UrlEncoder.Encode(match.Groups[1].Value).Replace('-', '_') + "_UTF8Encoded");
+
+        [Required]
+        public string Input { get; set; }
+        [Output]
+        public string Output { get; set; }
+
+        public override bool Execute()
+        {
+            try
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(Output));
+                File.WriteAllText(Output, Encode(File.ReadAllText(Input)));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.Log.LogErrorFromException(ex);
+                return false;
+            }
+        }
+    }
+}
