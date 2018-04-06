78db56389b7f51f8d710b0d4af4da3616e4683e1
thautwarm <twshere@outlook.com>
2018-04-06 13:33:56 +0800

    implement-code-gen(unfinished)

--- src/Modules/Trinity.FFI/SwigGen/Gen.fs
+++ src/Modules/Trinity.FFI/SwigGen/Gen.fs
@@ -8,11 +8,30 @@ module TGEN =
     open Trinity.Storage
     open SwigGen.Command
     open SwigGen.Operator
+    let manglingChar = "蛤"
             
-        
+    let mangling (name: string) = name.Replace(manglingChar, manglingChar + manglingChar)
+
+    let MakeName (typeDesc :TypeDescriptor): string = 
+        raise (NotImplementedException())
+
+    let makeMethod ()
+
         
     let Definition'Struct (struct': TypeDescriptor): string = 
-        raise (NotImplementedException(""))
+        PString.format "
+void {TypeName}{manglingChar}{FieldName}{manglingChar}Get_{Attrs}(void* struct_ptr, void* &data){{
+        
+        
+}}
+    
+
+"            (
+                Map [ 
+                    
+                    "manglingChar" ->> manglingChar
+                    "1" ->> "2"
+                ])
     
     let Definition'CellType(cellType': TypeDescriptor): string = 
         raise (NotImplementedException(""))
@@ -26,20 +45,41 @@ module TGEN =
             | STRUCT -> Definition'Struct anyType
             | LIST   -> Definition'GenericList anyType
             | CELL   -> Definition'CellType anyType
-            | _      -> failwith "unknown typecode"
+            | _      -> failwith "unexpected type"
         
     
-    let TypeInfer(anyType: TypeDescriptor): List<TypeDescriptor> = raise (NotImplementedException(""))
+    let TypeInfer(anyType: TypeDescriptor): List<TypeDescriptor> = 
     (** inference out the descriptors of struct types and generic list types in a cell descriptor.**)
+        match anyType with 
+            | {TypeCode=LIST; ElementType = elemType} 
+                 -> raise (NotImplementedException())
+            | {TypeCode=CELL; Members=members}
+            | {TypeCode=STRUCT; Members=members} 
+                 -> 
+                    members
+                    |> List.ofSeq
+                    |> List.map (fun field -> field.Type)
+                    |> fun tail -> anyType::tail
+            | _  -> failwith "unexpected type"
     
-    
-    let GenerateSrcCode(schema : IStorageSchema) : string = 
-        let partial f x y = f (x, y)
         
+    
+    let GenerateSrcCode(schema : IStorageSchema, 
+                        generatedTypeNames: System.Collections.Generic.HashSet<string>) : string = 
+        let partial f x y = f (x, y)
+        let filterAndUpdate (typeDesc: TypeDescriptor) = 
+            if typeDesc.TypeName |> generatedTypeNames.Contains
+            then 
+                false
+            else
+                generatedTypeNames.Add typeDesc.TypeName |> ignore
+                true
+
         schema.CellDescriptors
                 |> List.ofSeq
                 |> List.map (Builder.Make >> TypeInfer)
-                |> List.collect (List.map Transpile)
+                |> List.collect (List.filter (fun t -> generatedTypeNames.Contains t.TypeName))
+                |> List.map Transpile
                 |> partial System.String.Join "\n"
 
    
