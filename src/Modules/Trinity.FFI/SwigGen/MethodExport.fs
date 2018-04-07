cc879fd627279cfa0b49a35b453f958505c11a1d
thautwarm <twshere@outlook.com>
2018-04-07 15:19:20 +0800

    FFI.SwigGen: Gen.fs, refactored. implement

--- src/Modules/Trinity.FFI/SwigGen/MethodExport.fs
+++ src/Modules/Trinity.FFI/SwigGen/MethodExport.fs
@@ -5,6 +5,15 @@ open SwigGen
 module MethodExport = 
     open SwigGen.TGEN
     open GraphEngine.Jit.TypeSystem
+    open GraphEngine.Jit.JitCompiler
+    open GraphEngine.Jit
+    open System
 
+    let MethodGen (methodInfoSeq: seq<FunctionDescriptor * CodeGenerator>) : seq<NativeFunction * SwigCode> = seq{
+        for fnDesc, codeGenerator in methodInfoSeq do
+            let nativeFn: NativeFunction =  CompileFunction fnDesc
+            let swigCode: SwigCode      =  nativeFn.CallSite.ToString() |> codeGenerator
+            yield (nativeFn, swigCode) 
+    }
     
 
