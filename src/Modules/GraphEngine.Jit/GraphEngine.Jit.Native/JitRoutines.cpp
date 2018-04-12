80fc4a3ba5f0c45f441768570149e03fa7102de8
Yatao Li <yatli@microsoft.com>
2018-04-12 21:07:13 +0800

    GraphEngine.Jit.Native: code refactoring on type safety; implement tsl_assign

--- src/Modules/GraphEngine.Jit/GraphEngine.Jit.Native/JitRoutines.cpp
+++ src/Modules/GraphEngine.Jit/GraphEngine.Jit.Native/JitRoutines.cpp
@@ -1,6 +1,7 @@
 #include "GraphEngine.Jit.Native.h"
 #include <cstring>
 #include <algorithm>
+#include <functional>
 
 extern "C" char* tsl_getstring(int32_t* trinity_string_ptr)
 {
@@ -28,16 +29,16 @@ extern "C" void* tsl_copy_dynamic(int32_t* ptr)
     return tsl_copy((char*)ptr, len);
 }
 
-extern "C" void tsl_assign_fixed(char* dst, char* src, int32_t size)
-{
-}
-
 extern "C" void tsl_assign(CellAccessor* accessor, char* dst, char* src, int32_t size_dst, int32_t size_src)
 {
-}
+    if (size_dst != size_src) 
+    {
+        auto offset = dst - accessor->cellPtr;
+        Storage::LocalMemoryStorage::ResizeCell(accessor->cellId, accessor->entryIndex, offset, size_src - size_dst, accessor->cellPtr);
+        dst = accessor->cellPtr + offset;
+    }
 
-extern "C" void tsl_assign_dynamic(CellAccessor* accessor, char* dst, char* src)
-{
+    memcpy(dst, src, size_src);
 }
 
 extern "C" void tsl_setstring(CellAccessor* accessor, int32_t* p, u16char* str)
@@ -57,6 +58,30 @@ extern "C" char* tsl_setu8string(CellAccessor* accessor, int32_t* p, char* trini
 
     *p = slen;
     tsl_assign(accessor, (char*)(p + 1), (char*)str.c_str(), tlen, slen);
+}
+
+
+// type-safe call
+template <typename TRET, typename... TARGS>
+CCFuncCall* safecall(X86Compiler &cc, TRET(TARGS...) func)
+{
+    return cc.call(imm_ptr(func), FuncSignatureT<TRET, TARGS...>());
+}
+
+void push(X86Compiler &cc, X86Gp& reg, std::vector<int32_t>& plan)
+{
+    for (auto i : plan)
+    {
+        if (i > 0) 
+        {
+            cc.add(reg, i);
+        }
+        else
+        {
+            cc.add(reg, x86::dword_ptr(reg));
+            cc.add(reg, sizeof(int32_t));
+        }
+    }
 }
 
 std::vector<int32_t>&& walk(TypeDescriptor* type, MemberDescriptor* this_member = nullptr)
@@ -106,13 +131,13 @@ JitRoutine(BGet)
 
     if (tc == TypeCode::TC_STRING)
     {
-        auto call = cc.call(imm_ptr(tsl_getstring), FuncSignature1<void*, void*>());
+        auto call = safecall(cc, tsl_getstring);
         call->setArg(0, ctx.cellPtr);
         call->setRet(0, retreg);
     }
     else if (tc == TypeCode::TC_U8STRING)
     {
-        auto call = cc.call(imm_ptr(tsl_getu8string), FuncSignature1<void*, void*>());
+        auto call = safecall(cc, tsl_getu8string);
         call->setArg(0, ctx.cellPtr);
         call->setRet(0, retreg);
     }
@@ -128,7 +153,7 @@ JitRoutine(BGet)
         if (plan.size() == 0) return; // empty struct
         else if (plan.size() == 1 && plan[0] == -1) // jump
         {
-            auto call = cc.call(imm_ptr(tsl_copy_dynamic), FuncSignature1<void*, void*>());
+            auto call = safecall(cc, tsl_copy_dynamic);
             call->setArg(0, ctx.cellPtr);
             call->setRet(0, retreg);
         }
@@ -137,21 +162,19 @@ JitRoutine(BGet)
             // void* p = cellPtr;
             auto p = cc.newGpq("p");
             cc.mov(p, ctx.cellPtr);
-            for (auto i : plan)
-            {
-                if (i > 0) { cc.add(p, i); }
-                else { cc.add(p, x86::dword_ptr(p)); }
-            }
+
+            push(cc, p, plan);
+
             // p at end, calculate len
             cc.sub(p, ctx.cellPtr);
-            auto call = cc.call(imm_ptr(tsl_copy), FuncSignature2<void*, void*, int32_t>());
+            auto call = safecall(cc, tsl_copy);
             call->setArg(0, ctx.cellPtr);
             call->setArg(1, p.as<X86Gpd>());
             call->setRet(0, retreg);
         }
         else //fixed copying
         {
-            auto call = cc.call(imm_ptr(tsl_copy), FuncSignature2<void*, void*, int32_t>());
+            auto call = safecall(cc, tsl_copy);
             call->setArg(0, ctx.cellPtr);
             call->setArg(1, imm(plan[0]));
             call->setRet(0, retreg);
@@ -171,14 +194,14 @@ JitRoutine(BSet)
 
     if (tc == TypeCode::TC_STRING)
     {
-        auto call = cc.call(imm_ptr(tsl_setstring), FuncSignature3<void, void*, void*, void*>());
+        auto call = safecall(cc, tsl_setstring);
         call->setArg(0, ctx.cellAccessor);
         call->setArg(1, ctx.cellPtr);
         call->setArg(2, src);
     }
     else if (tc == TypeCode::TC_U8STRING)
     {
-        auto call = cc.call(imm_ptr(tsl_setu8string), FuncSignature3<void, void*, void*, void*>());
+        auto call = safecall(cc, tsl_setstring);
         call->setArg(0, ctx.cellAccessor);
         call->setArg(1, ctx.cellPtr);
         call->setArg(2, src);
@@ -195,10 +218,25 @@ JitRoutine(BSet)
         if (plan.size() == 0) return; // empty struct
         else if (plan.size() == 1 && plan[0] == -1) // jump
         {
-            auto call = cc.call(imm_ptr(tsl_assign_dynamic), FuncSignature3<void, void*, void*, void*>());
+            auto src_len_gp = cc.newGpw("src_len");
+            auto dst_len_gp = cc.newGpw("dst_len");
+
+            //TODO optional
+
+            // load length headers
+            cc.mov(src_len_gp, x86::dword_ptr(src));
+            cc.mov(dst_len_gp, x86::dword_ptr(ctx.cellPtr));
+            // set length header of dst
+            cc.mov(x86::dword_ptr(dst_len_gp), src_len_gp);
+            cc.add(ctx.cellPtr, 4);
+            cc.add(src, 4);
+
+            auto call = safecall(cc, tsl_assign);
             call->setArg(0, ctx.cellAccessor);
             call->setArg(1, ctx.cellPtr);
             call->setArg(2, src);
+            call->setArg(3, dst_len_gp);
+            call->setArg(4, src_len_gp);
         }
         else if (plan.size() > 1) // walking
         {
@@ -208,20 +246,14 @@ JitRoutine(BSet)
             auto q = cc.newGpq("q");
             cc.mov(p, src);
             cc.mov(q, ctx.cellPtr);
-            for (auto i : plan)
-            {
-                if (i > 0) { cc.add(p, i); }
-                else { cc.add(p, x86::dword_ptr(p)); }
-            }
-            for (auto i : plan)
-            {
-                if (i > 0) { cc.add(q, i); }
-                else { cc.add(q, x86::dword_ptr(q)); }
-            }
+            push(cc, p, plan);
+            push(cc, q, plan);
             // p & q at end, calculate len
             cc.sub(p, src);
             cc.sub(q, ctx.cellPtr);
-            auto call = cc.call(imm_ptr(tsl_assign), FuncSignature5<void, void*, void*, void*, int32_t, int32_t>());
+
+            auto call = safecall(cc, tsl_assign);
+
             call->setArg(0, ctx.cellAccessor);
             call->setArg(1, ctx.cellPtr);
             call->setArg(2, src);
@@ -230,7 +262,7 @@ JitRoutine(BSet)
         }
         else //fixed copying
         {
-            auto call = cc.call(imm_ptr(tsl_assign_fixed), FuncSignature3<void, void*, void*, int32_t>());
+            auto call = safecall(cc, memcpy);
             call->setArg(0, ctx.cellPtr);
             call->setArg(1, src);
             call->setArg(2, imm(plan[0]));
@@ -243,11 +275,7 @@ JitRoutine(BSet)
 JitRoutine(SGet)
 {
     auto plan = walk(seq.ParentType(), seq.CurrentMember());
-    for (auto i : plan)
-    {
-        if (i > 0) { cc.add(ctx.cellPtr, i); }
-        else { cc.add(ctx.cellPtr, x86::dword_ptr(ctx.cellPtr)); }
-    }
+    push(cc, ctx.cellPtr, plan);
 }
 
 JitRoutine(SSet)
@@ -258,7 +286,7 @@ JitRoutine(SSet)
 
 JitRoutine(GSGet)
 {
-
+    //TODO reflection layer for generic interfaces
 }
 
 JitRoutine(GSSet)
@@ -272,11 +300,11 @@ JitRoutine(LGet)
     auto l = cc.newLabel();
     ctx.addArg(index);
     // jump over length header
-    cc.add(ctx.cellPtr, sizeof(int32_t)); 
+    cc.add(ctx.cellPtr, sizeof(int32_t));
 
     auto plan = walk(seq.CurrentType());
 
-    if (plan.size() == 1 && plan[0] > 0) 
+    if (plan.size() == 1 && plan[0] > 0)
     {
         //fixed element type
 
@@ -292,30 +320,27 @@ JitRoutine(LGet)
             size >>= 1;
         }
 
-        if (bits == 1) 
+        if (bits == 1)
         {
             cc.lea(ctx.cellPtr, x86::ptr(ctx.cellPtr, index, shift));
         }
-        else 
+        //TODO else if (bits == 2)
+        //maybe an opt pass for such things
+        else
         {
             auto eax = x86::eax;
             cc.imul(index, plan[0]);
             cc.lea(ctx.cellPtr, x86::ptr(index));
         }
-
     }
-    else 
+    else
     {
         //keep walking
         auto loop = cc.newGpd("loop");
         cc.xor_(loop, loop);
 
         cc.bind(l);
-        for (auto i : plan)
-        {
-            if (i > 0) { cc.add(ctx.cellPtr, i); }
-            else { cc.add(ctx.cellPtr, x86::dword_ptr(ctx.cellPtr)); }
-        }
+        push(cc, ctx.cellPtr, plan);
         cc.inc(loop);
         cc.cmp(loop, index);
         cc.jb(l);
--- src/Modules/GraphEngine.Jit/GraphEngine.Jit.Playground/Program.cs
+++ src/Modules/GraphEngine.Jit/GraphEngine.Jit.Playground/Program.cs
@@ -7,25 +7,10 @@ using System.Threading.Tasks;
 
 namespace GraphEngine.Jit.Playground
 {
-    struct C1
-    {
-        public string bar;
-    }
-
     unsafe class Program
     {
-        void* get_bar_c1(void* c1)
-        {
-            var h = GCHandle.FromIntPtr((IntPtr)c1);
-            var obj = (C1)h.Target;
-            return GCHandle.ToIntPtr(GCHandle.Alloc(obj.bar)).ToPointer();
-        }
-
         static void Main(string[] args)
         {
-            //IntPtr fn = IntPtr.Zero;
-            //Assembly.AsmJit.test(ref fn);
-            //Console.WriteLine(Assembly.AsmJit.test2(fn));
         }
     }
 }
