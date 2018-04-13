37d77f129814995c90d4bac0c9138484be045b1a
Yatao Li <yatli@microsoft.com>
2018-04-13 11:56:42 +0800

    update build script

--- src/Modules/GraphEngine.Jit/build.ps1
+++ src/Modules/GraphEngine.Jit/build.ps1
@@ -3,5 +3,5 @@ Import-Module -Force -WarningAction Ignore "$PSScriptRoot\..\..\..\tools\setenv.
 Restore-GitSubmodules
 Remove-GraphEngineCache -prefix "graphengine.jit"
 
-Invoke-MSBuild -proj "$TRINITY_JIT_ROOT\GraphEngine.Jit.Native\GraphEngine.Jit.Native.vcxproj" -config Release -platform x64
+Invoke-MSBuild -proj "$TRINITY_JIT_ROOT\GraphEngine.Jit.Native\GraphEngine.Jit.Native.vcxproj" -config Release-CoreCLR -platform x64
 New-Package    -proj "$TRINITY_JIT_ROOT\GraphEngine.Jit.sln"
