0fc81361ab6f09424bc0ae83ee0a667593c79cd1
Yatao Li <yatli@microsoft.com>
2018-04-27 22:38:55 +0800

    GraphEngine.Jit: remove CppSharpCodegen

--- src/Modules/GraphEngine.Jit/GraphEngine.Jit.CppSharpCodegen/Templates.h
+++
@@ -1,9 +0,0 @@
-﻿#include "../asmjit/src/asmjit/base/operand.h"
-
-using namespace asmjit;
-
-class TypeIdSpecializer
-{
-public:
-    static int LPVOID() { return (int)TypeIdOf<void*>::kTypeId; }
-};
