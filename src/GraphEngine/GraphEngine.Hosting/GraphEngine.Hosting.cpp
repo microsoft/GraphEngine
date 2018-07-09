a4a91281cbf855c30a3106bc7d336def4ce522fc
Yatao Li <yatli@microsoft.com>
2018-07-09 17:23:47 +0800

    add GraphEngine.Hosting project

---
+++ src/GraphEngine/GraphEngine.Hosting/GraphEngine.Hosting.cpp
@@ -0,0 +1,20 @@
+﻿// GraphEngine.Hosting.cpp : Defines the entry point for the application.
+//
+
+#include "TrinityCommon.h"
+#include "mscoree.h"
+
+#if defined(TRINITY_PLATFORM_WINDOWS)
+static const wchar_t* coreCLRDll = L"coreclr.dll";
+#elif defined(TRINITY_PLATFORM_LINUX)
+static const wchar_t* coreCLRDll = L"libcoreclr.so";
+#elif defined(TRINITY_PLATFORM_DARWIN)
+static const wchar_t* coreCLRDll = L"libcoreclr.dylib";
+#endif
+
+// sample host here: https://github.com/dotnet/samples/blob/master/core/hosting/host.cpp
+
+DLL_EXPORT TrinityErrorCode GraphEngineInitializeHosting(int argc, char** argv)
+{
+    return TrinityErrorCode::E_SUCCESS;
+}
---
+++ src/GraphEngine/GraphEngine.Hosting/GraphEngine.Hosting.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+
+#include "TrinityCommon.h"
+
+DLL_IMPORT TrinityErrorCode GraphEngineInitializeHosting(int argc, char** argv);
