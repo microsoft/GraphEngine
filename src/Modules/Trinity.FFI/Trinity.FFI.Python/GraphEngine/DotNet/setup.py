d4badf7ab95385f9b1e09a338e4de33507bb9913
thautwarm <twshere@outlook.com>
2018-07-19 21:27:41 +0800

    reisen: hosting

--- src/Modules/Trinity.FFI/Trinity.FFI.Python/GraphEngine/DotNet/setup.py
+++ src/Modules/Trinity.FFI/Trinity.FFI.Python/GraphEngine/DotNet/setup.py
@@ -28,6 +28,7 @@ class FilterDLL:
     def __init__(self):
         def is_dll(x: str):
             return x.lower().endswith('.dll')
+
         self.fn = Traversal.filter_by(is_dll)
 
     def __call__(self, collection):
@@ -45,24 +46,32 @@ def init_trinity_service() -> Module:
 
     cs_proj_file = module_dir.into("Dependencies.csproj")
 
-    print(cs_proj_file)
+    cmd_patterns = ["dotnet", "restore", f'"{cs_proj_file}"', '--packages', f'"{Env.nuget_root}"']
+
     # restore
-    call(["dotnet", "restore", f'"{cs_proj_file}"', '--packages', f'"{Env.nuget_root}"'])
+    if call(cmd_patterns):
+        raise EnvironmentError("DotNet restoring failed `{}`".format(' '.join(cmd_patterns).__repr__()))
 
     # search dlls
     with open(str(cs_proj_file)) as file:
         deps = [Dependency(package_name=ref.attrs['include'],
-                           version=ref.attrs["version"]).all()
+                           version=ref.attrs["version"]
+                           ).all()
                 for ref in
                 BeautifulSoup(file, "lxml").select('packagereference')
                 ] | Collect | FilterDLL
 
-    libs = [
-               Library('GraphEngine.{}'.format(module), version='2.0.9328', where='runtimes/win-x64/native').all()
-               for module in ['Core', 'FFI', 'Jit']
-           ] | Collect | FilterDLL
+    libs = [Library('GraphEngine.{}'.format(module),
+                    version='2.0.9328',
+                    where='runtimes/win-x64/native'
+                    ).all()
+            for module in
+            ['Core', 'FFI', 'Jit']
+            ] | Collect | FilterDLL
 
     sys.path.append(str(module_dir.parent()))
+
+    # TODO: native hosting
 
     for each_lib in libs:
         ctypes.cdll.LoadLibrary(each_lib)
@@ -71,20 +80,24 @@ def init_trinity_service() -> Module:
         clr.AddReference(each_dep)
 
     graph_engine_config_path = Env.graph_engine_config_path
-
+    # TODO
     __Trinity = __import__('Trinity')
 
+    # TODO
     __Trinity.TrinityConfig.StorageRoot = str(graph_engine_config_path.into('storage'))
 
     # __Trinity.TrinityConfig.LoggingLevel = __Trinity.Diagnostics.LogLevel.Info
 
+    # TODO
     __Trinity.TrinityConfig.LoadConfig(str(graph_engine_config_path.into("trinity.xml")))
 
+    # TODO: hosting
     __Trinity.Global.Initialize()
 
     __ffi = __import__('ffi')
 
     __ffi.Init()
+
 
     __import__('Trinity.Storage')
     __import__('Trinity.Storage.Composite')
