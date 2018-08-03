df4a8122b5415ab5974c0d28350a0a6788a4922f
Yatao Li <yatli@microsoft.com>
2018-08-03 15:10:45 +0800

    cmake: cleanup; add linux build script

--- src/Modules/GraphEngine.Jit/build.ps1
+++
@@ -1,6 +0,0 @@
-Import-Module -Force -WarningAction Ignore "$PSScriptRoot\..\..\..\tools\setenv.psm1"
-
-Restore-GitSubmodules
-Remove-GraphEngineCache -prefix "graphengine.jit"
-
-New-Package    -proj "$TRINITY_JIT_ROOT\GraphEngine.Jit.sln"
--- tools/build.ps1
+++ tools/build.ps1
@@ -4,19 +4,9 @@ Remove-Build
 Remove-GraphEngineCache
 
 mkdir "$REPO_ROOT\build" -Force 
-Set-Location "$REPO_ROOT\build"
+mkdir "$REPO_ROOT\bin"   -Force 
+cd "$REPO_ROOT\build"
+
+Register-LocalRepo
 cmake -G "Visual Studio 15 2017 Win64" --host=x64 $REPO_ROOT
 cmake --build . --config Release
-
-New-Package -proj $TRINITY_CORE_SLN
-Register-LocalRepo
-Remove-GraphEngineCache -prefix "GraphEngine.Core"
-
-New-Package -proj $LIKQ_SLN
-Invoke-Sub "$TRINITY_CLIENT_ROOT\build.ps1"
-New-Package -proj $TRINITY_STORAGE_COMPOSITE_SLN
-New-Package -proj $TRINITY_DYNAMICCLUSTER_SLN
-Invoke-Sub "$TRINITY_SERVICE_FABRIC_ROOT\build.ps1"
-Invoke-Sub "$SPARK_MODULE_ROOT\build.ps1"
-Invoke-Sub "$TRINITY_JIT_ROOT\build.ps1"
-Invoke-Sub "$TRINITY_FFI_ROOT\build.ps1"
