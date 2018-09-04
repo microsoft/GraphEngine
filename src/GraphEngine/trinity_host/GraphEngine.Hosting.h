a73093102d6053a48383a2aeb4a641b13e3aa096
Yatao Li <yatli@microsoft.com>
2018-09-05 00:49:33 +0800

    move out clr host

--- src/GraphEngine/trinity_host/GraphEngine.Hosting.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-
-#include "TrinityCommon.h"
-
-#if !defined(TRINITYC_EXPORTS)
-DLL_IMPORT TrinityErrorCode GraphEngineInit(IN int n_apppaths, IN char** lp_apppaths, OUT void*& lpenv);
-DLL_IMPORT TrinityErrorCode GraphEngineUninit(IN const void* lpenv);
-DLL_IMPORT TrinityErrorCode GraphEngineGetFunction(IN const void* lpenv, IN char* lp_entry_asm, IN char* lp_entry_class, IN char* lp_entry_method, OUT void** lp_func);
-#endif
