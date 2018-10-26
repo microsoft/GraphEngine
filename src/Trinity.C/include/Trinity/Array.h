d941e9c0e91688780d7cd56222545ea18dc3dba4
Yatao Li <yatli@microsoft.com>
2018-10-26 23:18:33 +0800

    Trinity.C: BCL: Array: wrapping up. make sure we properly release the resources in various assignments/movements

--- src/Trinity.C/include/Trinity/Array.h
+++ src/Trinity.C/include/Trinity/Array.h
@@ -12,37 +12,44 @@ namespace Trinity
     {
         T* _array;
         std::allocator<T> _allocator;
-        size_t _length;
+        size_t _count;
     public:
         typedef Array<T> _Myt;
 
-        Array() : _length(0), _array(NULL) {}
-        Array(size_t length) : _length(length)
+        Array() : _count(0), _array(NULL) {}
+
+        Array(size_t count)
         {
-            allocate(); initialize();
+            allocate(count); 
+            initialize();
         }
-        Array(size_t length, std::function<T&&(int)> generator)
-            : _length(length)
+
+        Array(size_t count, std::function<T&&(int)> generator)
+            :
         {
-            allocate();
+            allocate(count);
             int idx = 0;
-            for (T *p = _array, *e = _array + _length; p != e; ++p)
+            for (T *p = _array, *e = _array + _count; p != e; ++p)
             {
                 _allocator.construct(p, generator(idx++));
             }
         }
+
         Array(const _Myt& arr)
         {
-            _copy_from(arr._array, arr._length);
+            _copy_from(arr._array, arr._count);
         }
+
         template <class _VecAlloc> Array(const std::vector<T, _VecAlloc> & vec)
         {
             _copy_from(vec.data(), vec.size());
         }
+
         Array(std::initializer_list<T> il)
         {
             _copy_from(il.begin(), il.size());
         }
+
         Array(_Myt&& arr) 
         { 
             _move_from(std::forward<_Myt>(arr)); 
@@ -50,22 +57,46 @@ namespace Trinity
 
         ~Array()
         {
-            if (_array)
-            {
-                destroy();
-                deallocate();
-            }
+            destroy();
+            deallocate();
         }
 
-        Array& operator = (const _Myt &  arr) { deallocate(); _copy_from(arr._array, arr._length); return *this; }
-        template <class _VecAlloc> Array& operator = (const std::vector<T, _VecAlloc> &  vec) { deallocate(); _copy_from(vec.data(), vec.size()); return *this; }
-        Array& operator = (std::initializer_list<T>  il) { deallocate(); _copy_from(il.begin(), il.size()); return *this; }
-        Array& operator = (_Myt && arr) { _move_from(std::forward<_Myt>(arr)); return *this; }
+        Array& operator = (const _Myt &  arr) 
+        {
+            destroy();
+            deallocate(); 
+            _copy_from(arr._array, arr._count); 
+            return *this; 
+        }
+
+        template <class _VecAlloc> Array& operator = (const std::vector<T, _VecAlloc> &  vec) 
+        { 
+            destroy();
+            deallocate();
+            _copy_from(vec.data(), vec.size());
+            return *this;
+        }
+
+        Array& operator = (std::initializer_list<T>  il) 
+        { 
+            destroy();
+            deallocate();
+            _copy_from(il.begin(), il.size()); 
+            return *this; 
+        }
+
+        Array& operator = (_Myt && arr) 
+        {
+            destroy();
+            deallocate(); 
+            _move_from(std::forward<_Myt>(arr));
+            return *this; 
+        }
 
         std::vector<T> ToList() const { std::vector<T> ret; ret.insert(ret.begin(), this->begin(), this->end()); return ret; }
         std::vector<T> ToList() { std::vector<T> ret; ret.insert(ret.begin(), this->begin(), this->end()); return ret; }
 
-        inline size_t Length() const { return _length; }
+        inline size_t Length() const { return _count; }
 
         //element access
         //T& operator [](size_t pos) { return _array[pos]; }
@@ -84,13 +115,13 @@ namespace Trinity
         //   into the new buffer. 
         T* detach_data() 
         { 
-            auto len = _length * sizeof(T);
+            auto len = _count * sizeof(T);
             auto ret = (T*)malloc(len);
 
-            std::move(_array, _array + _length, ret);
+            std::move(_array, _array + _count, ret);
 
-            _array = nullptr; 
-            _length = 0; 
+            //! deallocate, not destroy
+            deallocate();
             return ret; 
         }
 
@@ -99,68 +130,85 @@ namespace Trinity
         typedef const T *const_iterator;
 
         iterator begin() { return _array; }
-        iterator end() { return _array + _length; }
+        iterator end() { return _array + _count; }
         const_iterator begin() const { return _array; }
-        const_iterator end() const { return _array + _length; }
+        const_iterator end() const { return _array + _count; }
 
         const_iterator cbegin() { return _array; }
-        const_iterator cend() { return _array + _length; }
+        const_iterator cend() { return _array + _count; }
 
 
     private:
 
-        inline void allocate() { _array = _allocator.allocate(_length); }
-        inline void deallocate() { _allocator.deallocate(_array, _length); }
+        inline void allocate(size_t count) 
+        { 
+            _count = count;
+            _array = _allocator.allocate(_count); 
+        }
+
+        inline void deallocate() 
+        { 
+            _allocator.deallocate(_array, _count); 
+            _array = nullptr; 
+            _count = 0; 
+        }
 
         inline void initialize()
         {
-            if (std::is_trivial<T>::value)
-                memset(_array, 0, sizeof(T) * _length);
-            else/*forcibly call the constructor*/
-                for (T *p = _array, *e = _array + _length; p != e; ++p)
+            if constexpr(std::is_trivial<T>::value)
+                memset(_array, 0, sizeof(T) * _count);
+            else
+            {
+                /*forcibly call the constructor*/
+                for (T *p = _array, *e = _array + _count; p != e; ++p)
                     _allocator.construct(p);
+            }
         }
+
         inline void destroy()
         {
-            if ((std::is_trivial<T>::value || std::is_trivially_destructible<T>::value))
-                /*do nothing*/;
+            if constexpr ((std::is_trivial<T>::value || std::is_trivially_destructible<T>::value))
+            {
+                /*do nothing*/
+            }
             else
-                for (T *p = _array, *e = _array + _length; p != e; ++p)
-                    /*forcibly call destructor*/
+            {
+                /*forcibly call destructor*/
+                for (T *p = _array, *e = _array + _count; p != e; ++p)
+                {
                     _allocator.destroy(p);
-
-            /* alternative implementation:
-               std::_Destroy_range(_array, _array + _length, _allocator); */
+                }
+            }
         }
 
         inline void _move_from(_Myt&& arr)
         {
-            _length = arr._length;
+            _count = arr._count;
             _array = arr._array;
-            arr._array = NULL;
+            arr._array = nullptr;
+            arr._count = 0;
         }
 
-        inline void _copy_from(const T* source, size_t _len)
+        inline void _copy_from(const T* source, size_t cnt)
         {
             /* constructing from another array means deep copy */
-            _length = _len;
-            allocate();
-            if (std::is_trivially_copyable<T>::value)
+            allocate(cnt);
+            if constexpr(std::is_trivially_copyable<T>::value)
             {
-                memcpy(_array, source, sizeof(T) * _length);
+                memcpy(_array, source, sizeof(T) * _count);
             }
             else
             {
                 /* non trivial, have to construct one by one */
                 T* target = _array;
-                while (_len--)
+                while (cnt--)
+                {
                     /* forcibly call the copy constructor
                        (raises error when the underlying object is not copy-constructable) */
-                       _allocator.construct(target++, *(source++));
+                    _allocator.construct(target++, *(source++));
+                }
             }
         }
-
-
     };
 }
 
