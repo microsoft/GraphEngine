5aa003ca53c4809af77bad934cc41c6e9b812ac9
Yatao Li <yatli@microsoft.com>
2018-11-08 22:45:19 +0800

    separate tests and build.

--- tools/build.ps1
+++ tools/build.ps1
@@ -7,4 +7,11 @@ Push-Location "$REPO_ROOT\build"
 
 cmake -G "Visual Studio 15 2017 Win64" --host=x64 $REPO_ROOT
 cmake --build . --config Release
+
+try 
+{
+    cmake --build . --config Release --target ctest_dotnet
+}
+catch { }
+
 Pop-Location
