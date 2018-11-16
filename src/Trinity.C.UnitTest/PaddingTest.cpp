b692550b3cfffbe72d26f6b4a5f7bf04961492a7
Yatao Li <yatli@microsoft.com>
2018-11-16 21:13:13 +0800

    Trinity.C: unit test fail because of wcout/cout incompatibility

--- src/Trinity.C.UnitTest/PaddingTest.cpp
+++ src/Trinity.C.UnitTest/PaddingTest.cpp
@@ -4,12 +4,12 @@
 #include "Storage/MemoryTrunk/MemoryTrunk.h"
 #include "Storage/MTHash/MTHash.h"
 
-TEST_CASE("MemoryTrunk is properly cache-aligned")
+TEST_CASE("MemoryTrunk is properly cache-aligned", "[memory]")
 {
     REQUIRE(sizeof(Storage::MemoryTrunk) == 128);
 }
 
-TEST_CASE("MTHash is properly cache-aligned")
+TEST_CASE("MTHash is properly cache-aligned", "[memory]")
 {
     REQUIRE(sizeof(Storage::MTHash) == 64);
 }
