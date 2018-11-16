1dcba328391e8ab83542bfa2d2cbf11288d9f5fc
Yatao Li <yatli@microsoft.com>
2018-11-16 21:13:13 +0800

    Trinity.C: unit test fail because of wcout/cout incompatibility

---
+++ src/Trinity.C.UnitTest/Utf8Test.cpp
@@ -0,0 +1,14 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <Trinity.h>
+#include <Trinity/IO/Console.h>
+
+TEST_CASE("Console::WriteLine does not crash the program", "[console]")
+{
+    Trinity::IO::Console::WriteLine("hello!");
+}
+
+TEST_CASE("Console::WriteLine properly output wide characters", "[console]")
+{
+    Trinity::IO::Console::WriteLine(L"你好!");
+}
---
+++ src/Trinity.C.UnitTest/catch_wrapper.hpp
@@ -0,0 +1,6 @@
+#include "catch.hpp"
+#include "Trinity/IO/Console.h"
+
+#ifdef CATCH_CONFIG_NOSTDOUT
+//TODO
+#endif
