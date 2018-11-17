8ac71fdc4348568664a97c8b12e545a375bfb0ab
Yatao Li <yatli@microsoft.com>
2018-11-17 12:04:49 +0800

    MTHash: properly align to cacheline. Fix PaddingTest

--- src/Trinity.C.UnitTest/PaddingTest.cpp
+++ src/Trinity.C.UnitTest/PaddingTest.cpp
@@ -13,3 +13,33 @@ TEST_CASE("MTHash is properly cache-aligned", "[memory]")
 {
     REQUIRE(sizeof(Storage::MTHash) == 64);
 }
+
+TEST_CASE("AddressTableEntry is properly cache-aligned", "[memory]")
+{
+    REQUIRE(sizeof(Storage::AddressTableEntry) == 8);
+}
+
+TEST_CASE("AddressTableEndPoint is properly cache-aligned", "[memory]")
+{
+    REQUIRE(sizeof(Storage::AddressTableEndPoint) == 8);
+}
+
+TEST_CASE("HeadGroup is properly cache-aligned", "[memory]")
+{
+    REQUIRE(sizeof(Storage::HeadGroup) == 8);
+}
+
+TEST_CASE("CellEntry is properly cache-aligned", "[memory]")
+{
+    REQUIRE(sizeof(CellEntry) == 8);
+}
+
+TEST_CASE("MTEntry is properly cache-aligned", "[memory]")
+{
+    REQUIRE(sizeof(MTEntry) == 16);
+}
+
+TEST_CASE("MTHashAllocationInfo is properly cache-aligned", "[memory]")
+{
+    REQUIRE(sizeof(Storage::MTHashAllocationInfo) == 16);
+}
