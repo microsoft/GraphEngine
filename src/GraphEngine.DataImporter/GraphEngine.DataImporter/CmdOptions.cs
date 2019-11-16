6eafe3831f2ce8ac2b01f28df3c91478b3384ad3
Tavi Truman <tavi@inknowworks.com>
2019-11-16 09:39:58 -0800

    Upgraded to the latest version of CommandLineParser and adjusted the code to reflect API changes.

--- src/GraphEngine.DataImporter/GraphEngine.DataImporter/CmdOptions.cs
+++ src/GraphEngine.DataImporter/GraphEngine.DataImporter/CmdOptions.cs
@@ -21,16 +21,16 @@ namespace GraphEngine.DataImporter
         [Option('o', "output", HelpText = "Specifies data import output directory for importing tasks, and specifies the output TSL file name for TSL generation tasks", Required = false)]
         public string Output { get; set; }
 
-        [Option('g', "generate_tsl", HelpText = "Generates TSL", MutuallyExclusiveSet = "Action")]
+        [Option('g', "generate_tsl", HelpText = "Generates TSL", Default = "Action")]
         public bool GenerateTSL { get; set; }
 
-        [Option('s', "sorted", HelpText = "Specifies that the data is already sorted/grouped by entities", DefaultValue = false)]
+        [Option('s', "sorted", HelpText = "Specifies that the data is already sorted/grouped by entities", Default = false)]
         public bool Sorted { get; set; }
 
         [Option("delimiter", HelpText = "Specifies the delimiter of CSV or TSV file", Required = false)]
         public char Delimiter { get; set; }
 
-        [Option("dtr_threshold", HelpText = "Specifies the threshold of dominating type ratio (between 0 and 1.0)", Required = false, DefaultValue = 1.0)]
+        [Option("dtr_threshold", HelpText = "Specifies the threshold of dominating type ratio (between 0 and 1.0)", Required = false, Default = 1.0)]
         public double DominatingTypeThreshold { get; set; }
 
         [Option('f', "fileFormat", HelpText = "Specifies the default file format for the input parser, when file format cannot be determined from the file extension", Required = false)]
@@ -39,13 +39,12 @@ namespace GraphEngine.DataImporter
         [Option("notrim", HelpText = "Specifies that the data fields in CSV/TSV files are not trimmed", Required = false)]
         public bool NoTrim { get; set; }
 
-        [Option('a', "tsl_assembly", HelpText = "Specifies the TSL assembly for data importing.", MutuallyExclusiveSet = "Action")]
+        [Option('a', "tsl_assembly", HelpText = "Specifies the TSL assembly for data importing.", Default = "Action")]
         public string TSLAssembly { get; set; }
 		
-        [ValueList(typeof(List<string>))]
+        [Value(0, Default = typeof(List<string>))]
         public IList<string> ExplicitFiles { get; set; }
 
-        [HelpOption]
         public string GetUsage()
         {
             var help = new HelpText
@@ -57,7 +56,6 @@ namespace GraphEngine.DataImporter
             help.AddPreOptionsLine("Import from files to Graph Engine storage.");
             help.AddPreOptionsLine(string.Format("Usage: {0} [-t tsl] [-d directory] [-o output_dir] [--delimiter delimiter] [-f file_format] [--notrim] [-a tsl_assembly|-g] [explicit files]", Path.GetFileName(Assembly.GetExecutingAssembly().Location)));
 
-            help.AddOptions(this);
             // todo scan for supported file types
             help.AddPostOptionsLine("Only files with .json, .csv, .tsv and .ntriples suffix are recognized.");
             help.AddPostOptionsLine("The file name of a data file will be used as the type for deserialization (except for RDF files).");
