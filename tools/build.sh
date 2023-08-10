ab6e235f95b62b2cf192e48c4c30681f6aa84822
Bin Shao <binshao@live.com>
2023-08-10 13:32:03 +0800

    Update build script (linux)

--- tools/build.sh
+++ tools/build.sh
@@ -2,23 +2,21 @@
 
 if [ "$REPO_ROOT" == "" ] ; then REPO_ROOT="$(readlink -f $(dirname $(readlink -f $0))/../)" ; fi
 
-# check for build environment, tools and libraries
-
-if [ "$(command -v cmake)" == "" ] ; 
-then 
+if [ "$(command -v cmake)" == "" ] ;
+then
 	echo "error: cmake not found." 1>&2
-	exit -1
+	exit
 fi
 
-if [ "$(command -v dotnet)" == "" ] ; 
-then 
+if [ "$(command -v dotnet)" == "" ] ;
+then
 	echo "error: dotnet not found." 1>&2
 	echo "see: https://www.microsoft.com/net/download/linux"
-	exit -1
+	exit
 fi
 
 # build
-mkdir -p "$REPO_ROOT/build" && pushd "$_" || exit -1
-cmake "$REPO_ROOT" -DCMAKE_BUILD_TYPE=Release || exit -1
-make -j || exit -1
+mkdir -p "$REPO_ROOT/build" && pushd "$_" || exit
+cmake "$REPO_ROOT" -DCMAKE_BUILD_TYPE=Release || exit
+make -j $(nproc) || exit
 popd
